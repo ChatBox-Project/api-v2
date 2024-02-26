3f5d6cefde85609336866d5433fbc27603097e79
daenv <daenvle69@gmail.com>
2024-02-26 23:07:05 +0700

    Add TypeORM module and entities to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './controllers/database/database.module';
 
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities/entity.provider';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TypeOrmModule.forFeature(entities),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
