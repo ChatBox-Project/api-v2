2161b097ba7489d3f5e05daea20e1859e10b03b3
daenv <daenvle69@gmail.com>
2024-02-26 21:28:25 +0700

    Add ConfigModule and DatabaseModule imports to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './controllers/database/database.module';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor(private dataSourse: DataSource) {}ƒ
+}
