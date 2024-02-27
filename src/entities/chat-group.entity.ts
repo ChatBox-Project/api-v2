9391b2e67e06b1e451491a0c1352c17ce0d1bed1
daenv <daenvle69@gmail.com>
2024-02-27 11:43:44 +0700

    Add ChatGroupEntity class

---
+++ src/entities/chat-group.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Index } from 'typeorm';
+import { BaseEntity } from './bases/base.entity';
+import { IChatGroupEntity } from './interfaces/chat-group.entity.interface';
+
+export class ChatGroupEntity extends BaseEntity implements IChatGroupEntity {
+  @Index('IX_ChatGroupEntity_GroupId')
+  @Column({ type: 'uuid', nullable: true })
+  groupId: string;
+  @Index('IX_ChatGroupEntity_ChatBoxId')
+  @Column({ type: 'uuid', nullable: true })
+  chatBoxId: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  groupName?: string;
+
+  @Column({ type: 'varchar', array: true, nullable: true })
+  groupMembers?: string[];
+
+  @Index('IX_ChatGroupEntity_GroupLeaderId')
+  @Column({ type: 'uuid', nullable: true })
+  groupLeaderId: string;
+}
