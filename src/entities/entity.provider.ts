ae31455a304863ad684f85c05cc6cf1dd4394f7b
daenv <daenvle69@gmail.com>
2024-02-27 15:04:57 +0700

    Add new entities to entity provider

--- src/entities/entity.provider.ts
+++ src/entities/entity.provider.ts
@@ -1,4 +1,17 @@
 import { AccountEntity } from './account.entity';
 import { ChatBoxEntity } from './chat-box.entity';
+import { ChatGroupEntity } from './chat-group.entity';
+import { GroupLeaderPermission } from './group-leader-permission.entity';
+import { MessageEntity } from './message.entity';
+import { PermissionEntity } from './permission.entity';
+import { RoleEntity } from './role.entity';
 
-export const entities = [AccountEntity, ChatBoxEntity];
+export const entities = [
+  AccountEntity,
+  ChatBoxEntity,
+  GroupLeaderPermission,
+  RoleEntity,
+  MessageEntity,
+  PermissionEntity,
+  ChatGroupEntity,
+];
