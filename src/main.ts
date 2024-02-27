244254ab9dc9ea23736670767807ab5ffdf37735
daenv <daenvle69@gmail.com>
2024-02-27 11:43:38 +0700

    Add global validation pipe and global prefix to app initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  app.use(passport.initialize());
+
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
