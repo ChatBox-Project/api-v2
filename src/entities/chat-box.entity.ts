b30330836f30cfcc92e291f94e707abf304e460b
daenv <daenvle69@gmail.com>
2024-02-29 18:29:33 +0700

    Add columns and relationship to ChatBoxEntity

--- src/entities/chat-box.entity.ts
+++ src/entities/chat-box.entity.ts
@@ -1,6 +1,19 @@
 import { IChatBoxEntity } from './interfaces/chat-box.entity.interface';
 import { BaseEntity } from './bases/base.entity';
-import { Column, Entity, Index } from 'typeorm';
+import { Column, Entity, Index, JoinColumn, OneToMany } from 'typeorm';
+import { UserBaseEntity } from './user.base.entity';
 
 @Entity({ name: 'chat-box' })
-export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {}
+export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {
+  @Index('IX_ChatBox_ChatBoxId', { unique: true })
+  @Column({ name: 'chat_box_id', type: 'varchar', length: 255 })
+  chatBoxId: string;
+
+  @Index('IX_ChatBox_ChatBoxName', { unique: true })
+  @Column({ name: 'chat_box_name', type: 'varchar', length: 255 })
+  chatBoxName: string;
+
+  @OneToMany(() => UserBaseEntity, (user) => user.chatBoxId)
+  @JoinColumn()
+  userId: UserBaseEntity;
+}
