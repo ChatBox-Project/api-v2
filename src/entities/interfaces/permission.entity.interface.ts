8fda6e9292ee23e0822522962ca5367c1a71c716
daenv <daenvle69@gmail.com>
2024-02-29 23:21:18 +0700

    Import GroupLeaderPermissionEntity in permission.entity.interface.ts

--- src/entities/interfaces/permission.entity.interface.ts
+++ src/entities/interfaces/permission.entity.interface.ts
@@ -1,7 +1,7 @@
+import { GroupLeaderPermissionEntity } from '../group-leader-permission.entity';
 import { IBaseEntity } from './base.entity.interface';
-import { IGroupLeaderPermission } from './group-leader-permission.entity.interface';
 
 export interface IPermission extends IBaseEntity {
   name: string;
-  groupLeaderPermissionId: IGroupLeaderPermission[];
+  groupLeaderPermission: GroupLeaderPermissionEntity[];
 }
