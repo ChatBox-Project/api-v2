4f24565db7c2a2e5c042bccdffeda4cf77afea49
daenv <daenvle69@gmail.com>
2024-02-29 18:29:52 +0700

    Add properties to MessageEntity interface

--- src/entities/message.entity.ts
+++ src/entities/message.entity.ts
@@ -1,7 +1,19 @@
 import { Column, Entity, Index } from 'typeorm';
 import { BaseEntity } from './bases/base.entity';
 import { IMessage } from './interfaces/message.entity.interface';
+import { IChatBoxEntity, IChatGroupEntity } from './interfaces';
 
 @Entity({ name: 'message' })
 export class MessageEntity extends BaseEntity implements IMessage {
- }
+  messageId?: string;
+  chatboxId?: string;
+  senderId?: string;
+  messageType?: string;
+  contentText?: string;
+  contentImage?: string;
+  contentAudio?: string;
+  contentVideo?: string;
+  contentFile?: string;
+  chatBoxId: IChatBoxEntity;
+  chatGroupId?: IChatGroupEntity;
+}
