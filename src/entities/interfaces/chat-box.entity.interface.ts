c865eb093804b0b637f9d142d3aa878fcf5cfb9c
daenv <daenvle69@gmail.com>
2024-02-29 23:21:03 +0700

    Refactor chat box entity interface

--- src/entities/interfaces/chat-box.entity.interface.ts
+++ src/entities/interfaces/chat-box.entity.interface.ts
@@ -1,8 +1,8 @@
+import { UserBaseEntity } from '../user.base.entity';
 import { IBaseEntity } from './base.entity.interface';
-import { IUserBaseEntity } from './user.base.entity.interface';
 
 export interface IChatBoxEntity extends IBaseEntity {
   chatBoxId: string;
   chatBoxName: string;
-  userId: IUserBaseEntity;
+  user: UserBaseEntity;
 }
