61bcf5e0169354594d7a3cdd1bd486faa8ae2bbc
daenv <daenvle69@gmail.com>
2024-02-29 23:20:33 +0700

    Refactor ChatGroupEntity class***

--- src/entities/chat-group.entity.ts
+++ src/entities/chat-group.entity.ts
@@ -11,53 +11,48 @@ import {
 import { BaseEntity } from './bases/base.entity';
 import { IChatGroupEntity } from './interfaces/chat-group.entity.interface';
 import { UserBaseEntity } from './user.base.entity';
-import { GroupLeaderPermission } from './group-leader-permission.entity';
 import { MessageEntity } from './message.entity';
+import { GroupLeaderPermissionEntity } from './group-leader-permission.entity';
 
 @Entity({ name: 'chat-group' })
 export class ChatGroupEntity extends BaseEntity implements IChatGroupEntity {
   @Index('IX_ChatGroup_GroupId', { unique: true })
-  @Column({ name: 'group_id', type: 'varchar', length: 255 })
-  @PrimaryGeneratedColumn('uuid')
-  groupId?: string;
-
-  @Index('IX_ChatGroup_ChatBoxId', { unique: true })
-  @Column({ name: 'chat_box_id', type: 'varchar', length: 255 })
-  @PrimaryGeneratedColumn('uuid')
-  chatBoxId?: string;
+  @Column({ name: 'group_id', type: 'uuid', length: 255 })
+  @PrimaryGeneratedColumn()
+  groupId: string;
 
   @Index('IX_ChatGroup_GroupName', { unique: true })
   @Column({ name: 'group_name', type: 'varchar', length: 255 })
-  groupName?: string;
+  groupName: string;
 
   @Column({ name: 'group_members', type: 'varchar', length: 255, array: true })
   groupMembers?: string[];
 
   @Column({ name: 'group_leader_id', type: 'varchar', length: 255 })
-  groupLeaderId: string;
+  groupLeader: string;
 
-  @ManyToMany(() => UserBaseEntity, (user) => user.chatGroupId)
+  @ManyToMany(() => UserBaseEntity, (user) => user.chatGroup)
   @JoinTable()
-  userId: UserBaseEntity[];
+  user: UserBaseEntity[];
 
-  @ManyToOne(() => GroupLeaderPermission, (gr) => gr.groupId)
+  @ManyToOne(() => GroupLeaderPermissionEntity, (gr) => gr.group)
   @JoinColumn()
-  groupLeaderPermissionId: GroupLeaderPermission;
+  groupLeaderPermission: GroupLeaderPermissionEntity;
 
-  @ManyToOne(() => MessageEntity, (mess) => mess.chatGroupId)
+  @ManyToOne(() => MessageEntity, (mess) => mess.chatGroup)
   @JoinColumn()
-  messageId?: MessageEntity;
+  message: MessageEntity[];
 
   constructor(props?: ChatGroupEntity) {
     super();
     if (props) {
       this.groupId = props.groupId;
-      this.chatBoxId = props.chatBoxId;
       this.groupName = props.groupName;
       this.groupMembers = props.groupMembers;
-      this.groupLeaderId = props.groupLeaderId;
-      this.userId = props.userId;
-      this.groupLeaderPermissionId = props.groupLeaderPermissionId;
+      this.groupLeader = props.groupLeader;
+      this.user = props.user;
+      this.groupLeaderPermission = props.groupLeaderPermission;
+      this.message = props.message;
     }
     Object.assign(this, props);
   }
