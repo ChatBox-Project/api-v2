d1e26e32418a238ab3393efe224c544720bad45a
daenv <daenvle69@gmail.com>
2024-02-29 18:29:41 +0700

    Refactor GroupLeaderPermission entity

--- src/entities/group-leader-permission.entity.ts
+++ src/entities/group-leader-permission.entity.ts
@@ -1,10 +1,35 @@
-import { Column, Entity, Index } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
 import { BaseEntity } from './bases/base.entity';
 import { IGroupLeaderPermission } from './interfaces/group-leader-permission.entity.interface';
+import { UserBaseEntity } from './user.base.entity';
+import { ChatGroupEntity } from './chat-group.entity';
+import { PermissionEntity } from './permission.entity';
 
 @Entity({ name: 'group-leader-permission' })
 export class GroupLeaderPermission
   extends BaseEntity
   implements IGroupLeaderPermission
 {
+  @Index('IX_GroupLeaderPermission_GroupLeaderPermissionId', { unique: true })
+  @Column({ name: 'group_leader_permission_id', type: 'varchar', length: 255 })
+  group_leader_permissionId?: string;
+
+  @OneToMany(() => UserBaseEntity, (user) => user.accountId)
+  @JoinColumn()
+  leaderId?: UserBaseEntity;
+
+  @OneToMany(() => ChatGroupEntity, (chatGroup) => chatGroup.groupId)
+  @JoinColumn()
+  groupId?: string | ChatGroupEntity[];
+
+  @OneToOne(() => PermissionEntity)
+  @JoinColumn()
+  permissionId: PermissionEntity;
 }
