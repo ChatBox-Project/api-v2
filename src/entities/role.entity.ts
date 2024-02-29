7a32a71770724270e79e6ba563f7d6aa537af602
daenv <daenvle69@gmail.com>
2024-02-29 17:04:59 +0700

    Remove unused properties from RoleEntity

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -4,8 +4,4 @@ import { IRole } from './interfaces/role.entity.interface';
 
 @Entity({ name: 'role' })
 export class RoleEntity extends BaseEntity implements IRole {
-  @Column({ type: 'uuid' })
-  roleId: string;
-  @Column({ type: 'varchar', length: 255 })
-  roleName: string;
-}
+ }
