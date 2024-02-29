365e74f81e921407b9a07539345e0d10532488ec
daenv <daenvle69@gmail.com>
2024-02-29 17:04:11 +0700

    Remove unnecessary properties from ChatBoxEntity

--- src/entities/chat-box.entity.ts
+++ src/entities/chat-box.entity.ts
@@ -3,25 +3,4 @@ import { BaseEntity } from './bases/base.entity';
 import { Column, Entity, Index } from 'typeorm';
 
 @Entity({ name: 'chat-box' })
-export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {
-  @Index('IX_ChatBoxEntity_UserId')
-  @Column({ type: 'uuid' })
-  userId: string[];
-
-  @Index('IX_ChatBoxEntity_ChatBoxId')
-  @Column({ type: 'uuid' })
-  chatBoxId: string;
-
-  @Column({ type: 'varchar', length: 255, default: '' })
-  chatBoxName: string;
-
-  constructor(props?: IChatBoxEntity) {
-    super();
-    if (props) {
-      this.userId = props.userId;
-      this.chatBoxId = props.chatBoxId;
-      this.chatBoxName = props.chatBoxName;
-    }
-    Object.assign(this, props);
-  }
-}
+export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {}
