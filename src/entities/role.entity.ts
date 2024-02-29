51654a3f6129d21f1553e251c3b63513449e2d65
daenv <daenvle69@gmail.com>
2024-02-29 20:03:09 +0700

    Add PrimaryGeneratedColumn to roleId in RoleEntity

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BaseEntity } from './bases/base.entity';
 import { IRole } from './interfaces/role.entity.interface';
 import { AccountEntity } from './account.entity';
@@ -7,6 +13,7 @@ import { RoleEnum } from 'src/configs/enums/role.enum';
 @Entity({ name: 'role' })
 export class RoleEntity extends BaseEntity implements IRole {
   @Column({ name: 'role_id', type: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   roleId: string;
   @Column({ name: 'role_name', enum: RoleEnum, default: RoleEnum.user })
   roleName: RoleEnum;
