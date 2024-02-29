0988e2e8b11c577f92245f1055804ad154616da5
daenv <daenvle69@gmail.com>
2024-02-29 17:05:33 +0700

    Add accountId property to IRole interface

--- src/entities/interfaces/role.entity.interface.ts
+++ src/entities/interfaces/role.entity.interface.ts
@@ -1,6 +1,9 @@
+import { IAccountEntity } from './account.entity.interface';
 import { IBaseEntity } from './base.entity.interface';
 
 export interface IRole extends IBaseEntity {
   roleId: string;
   roleName: string;
+
+  accountId: IAccountEntity;
 }
