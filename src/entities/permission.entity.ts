591593ae17754e89848d2524a24e89019e79aa4a
daenv <daenvle69@gmail.com>
2024-02-29 17:04:56 +0700

    Remove name property from PermissionEntity constructor

--- src/entities/permission.entity.ts
+++ src/entities/permission.entity.ts
@@ -4,14 +4,4 @@ import { BaseEntity } from './bases/base.entity';
 
 @Entity({ name: 'permission' })
 export class PermissionEntity extends BaseEntity implements IPermission {
-  @Column({ type: 'varchar', length: 255 })
-  name: string;
-
-  constructor(props?: IPermission) {
-    super();
-    if (props) {
-      this.name = props.name;
-    }
-    Object.assign(this, props);
-  }
 }
