bebf311aab327419be1425a46b8b82d41560b463
daenv <daenvle69@gmail.com>
2024-02-29 19:12:00 +0700

    Add validation and relationship to GroupLeaderPermission in PermissionEntity

--- src/entities/permission.entity.ts
+++ src/entities/permission.entity.ts
@@ -1,10 +1,17 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 import { IPermission } from './interfaces/permission.entity.interface';
 import { BaseEntity } from './bases/base.entity';
 import { IGroupLeaderPermission } from './interfaces';
+import { IsNotEmpty } from 'class-validator';
+import { GroupLeaderPermission } from './group-leader-permission.entity';
 
 @Entity({ name: 'permission' })
 export class PermissionEntity extends BaseEntity implements IPermission {
+  @Column({ name: 'name', type: 'varchar', length: 255 })
+  @IsNotEmpty()
   name: string;
+
+  @OneToOne(() => GroupLeaderPermission, (gr) => gr.group_leader_permissionId)
+  @JoinColumn()
   groupLeaderPermissionId: IGroupLeaderPermission[];
 }
