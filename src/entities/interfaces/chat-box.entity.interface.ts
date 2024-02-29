b2dab1d9fb5264de4cbb5f0c4887fe620c77db50
daenv <daenvle69@gmail.com>
2024-02-29 17:05:11 +0700

    Add IUserBaseEntity import to IChatBoxEntity interface

--- src/entities/interfaces/chat-box.entity.interface.ts
+++ src/entities/interfaces/chat-box.entity.interface.ts
@@ -1,7 +1,9 @@
 import { IBaseEntity } from './base.entity.interface';
+import { IUserBaseEntity } from './user.base.entity.interface';
 
 export interface IChatBoxEntity extends IBaseEntity {
   chatBoxId: string;
   chatBoxName: string;
-  userId: string[];
+
+  userId: IUserBaseEntity;
 }
