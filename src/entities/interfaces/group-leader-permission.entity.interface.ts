b9f623c8bd23783a36a1e976410238b64540a1e2
daenv <daenvle69@gmail.com>
2024-02-29 17:05:18 +0700

    Refactor group leader permission entity interface

--- src/entities/interfaces/group-leader-permission.entity.interface.ts
+++ src/entities/interfaces/group-leader-permission.entity.interface.ts
@@ -1,8 +1,12 @@
 import { IBaseEntity } from './base.entity.interface';
+import { IChatGroupEntity } from './chat-group.entity.interface';
+import { IPermission } from './permission.entity.interface';
+import { IUserBaseEntity } from './user.base.entity.interface';
 
 export interface IGroupLeaderPermission extends IBaseEntity {
-  groupId?: string;
-  leaderId?: string;
-  permissionId?: string;
   group_leader_permissionId?: string;
+
+  leaderId?: IUserBaseEntity;
+  groupId?: IChatGroupEntity[] | string;
+  permissionId: IPermission;
 }
