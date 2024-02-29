a18c2717035599cf35aa26d4745365b66d024f27
daenv <daenvle69@gmail.com>
2024-02-29 17:05:37 +0700

    Add account, chat box, chat group, and group leader permission interfaces to user base entity

--- src/entities/interfaces/user.base.entity.interface.ts
+++ src/entities/interfaces/user.base.entity.interface.ts
@@ -1,12 +1,20 @@
 import { EGender } from 'src/configs';
 import { IBaseEntity } from './base.entity.interface';
+import { IAccountEntity } from './account.entity.interface';
+import { IChatBoxEntity } from './chat-box.entity.interface';
+import { IChatGroupEntity } from './chat-group.entity.interface';
+import { IGroupLeaderPermission } from './group-leader-permission.entity.interface';
 
 export interface IUserBaseEntity extends IBaseEntity {
-  userId: string;
   firstName: string;
   lastName: string;
   phone: number;
   sex: EGender;
   avatarUrl: string;
   birth: Date;
+
+  accountId: IAccountEntity;
+  chatBoxId?: IChatBoxEntity[];
+  chatGroupId?: IChatGroupEntity[];
+  groupLeaderPermissionId?: IGroupLeaderPermission[];
 }
