ed4b383e82d3a06f0d272b3b764869b47c17afcb
daenv <daenvle69@gmail.com>
2024-02-29 18:30:07 +0700

    Refactor account entity interface

--- src/entities/interfaces/account.entity.interface.ts
+++ src/entities/interfaces/account.entity.interface.ts
@@ -1,21 +1,20 @@
 import { IIdEntity } from './id.entity.interface';
 import { IRole } from './role.entity.interface';
+import { IUserBaseEntity } from './user.base.entity.interface';
 
 export interface IAccountEntity extends IIdEntity {
   username: string;
   email: string;
   password: string;
   passwordSalt: string;
-  firstName: string;
 
   jwtToken: string;
   refreshToken: string;
   verified: boolean;
-  roles: string[];
   verificationExpires: Date;
   loginAttempts: number;
   blockExpires: Date;
 
   role?: IRole;
-  account: IAccountEntity;
+  userId: IUserBaseEntity;
 }
