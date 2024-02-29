1ddce561b3d4687a63dd2ae3cf983b229dcadfcd
daenv <daenvle69@gmail.com>
2024-02-29 18:29:56 +0700

    Add name and groupLeaderPermissionId properties to PermissionEntity

--- src/entities/permission.entity.ts
+++ src/entities/permission.entity.ts
@@ -1,7 +1,10 @@
 import { Column, Entity } from 'typeorm';
 import { IPermission } from './interfaces/permission.entity.interface';
 import { BaseEntity } from './bases/base.entity';
+import { IGroupLeaderPermission } from './interfaces';
 
 @Entity({ name: 'permission' })
 export class PermissionEntity extends BaseEntity implements IPermission {
+  name: string;
+  groupLeaderPermissionId: IGroupLeaderPermission[];
 }
