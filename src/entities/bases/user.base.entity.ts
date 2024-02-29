1238cad25d6351ff76c16c683b3a365dc691c748
daenv <daenvle69@gmail.com>
2024-02-29 16:12:51 +0700

    Delete UserBaseEntity and update IUserBaseEntity interface

--- src/entities/bases/user.base.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Index } from 'typeorm';
-import { IdEntity } from './id.entity';
-
-export abstract class UserBaseEntity extends IdEntity {
-  @Index({ unique: true })
-  @Column({ type: 'uuid' })
-  userId: string;
-}
--- src/entities/interfaces/user.base.entity.interface.ts
+++ src/entities/interfaces/user.base.entity.interface.ts
@@ -1,5 +1,12 @@
-import { IIdEntity } from './id.entity.interface';
+import { EGender } from 'src/configs';
+import { IBaseEntity } from './base.entity.interface';
 
-export interface IUserBaseEntity extends IIdEntity {
+export interface IUserBaseEntity extends IBaseEntity {
   userId: string;
+  firstName: string;
+  lastName: string;
+  phone: number;
+  sex: EGender;
+  avatarUrl: string;
+  birth: Date;
 }
