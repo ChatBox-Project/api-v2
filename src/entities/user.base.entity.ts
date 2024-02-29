bc5c4f25054d8503850d88b260afdc870f0414f8
daenv <daenvle69@gmail.com>
2024-02-29 18:30:04 +0700

    Add properties and relationships to UserBaseEntity

--- src/entities/user.base.entity.ts
+++ src/entities/user.base.entity.ts
@@ -2,6 +2,23 @@ import { Column, Entity, Index } from 'typeorm';
 import { IUserBaseEntity } from './interfaces/user.base.entity.interface';
 import { EGender } from 'src/configs';
 import { BaseEntity } from './bases/base.entity';
+import {
+  IAccountEntity,
+  IChatBoxEntity,
+  IChatGroupEntity,
+  IGroupLeaderPermission,
+} from './interfaces';
 
 @Entity({ name: 'user_base' })
-export class UserBaseEntity extends BaseEntity implements IUserBaseEntity {}
+export class UserBaseEntity extends BaseEntity implements IUserBaseEntity {
+  firstName: string;
+  lastName: string;
+  phone: number;
+  sex: EGender;
+  avatarUrl: string;
+  birth: Date;
+  accountId: IAccountEntity;
+  chatBoxId?: IChatBoxEntity[];
+  chatGroupId?: IChatGroupEntity[];
+  groupLeaderPermissionId?: IGroupLeaderPermission[];
+}
