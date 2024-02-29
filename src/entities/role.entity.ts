589e6257a649be8efc8ccc06ab5cdfdb119f7792
daenv <daenvle69@gmail.com>
2024-02-29 18:30:00 +0700

    Add roleId, roleName, and accountId properties to RoleEntity

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,7 +1,11 @@
 import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './bases/base.entity';
 import { IRole } from './interfaces/role.entity.interface';
+import { IAccountEntity } from './interfaces';
 
 @Entity({ name: 'role' })
 export class RoleEntity extends BaseEntity implements IRole {
- }
+  roleId: string;
+  roleName: string;
+  accountId: IAccountEntity;
+}
