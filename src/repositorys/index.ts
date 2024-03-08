3955b04e673a735376d8f6ffd40e367f2f70cc5f
daenv <daenvle69@gmail.com>
2024-03-08 19:54:02 +0700

    Add repository files

---
+++ src/repositorys/index.ts
@@ -0,0 +1,7 @@
+export * from './account.repository';
+export * from './message.repository';
+export * from './permission.entity';
+export * from './group-leader-permission.repository';
+export * from './role.repository';
+export * from './chat-box.entity';
+export * from './chat-group.repository';
