61079daa8a1d67358a69cd155df252a902b44cb0
daenv <daenvle69@gmail.com>
2024-03-08 19:53:06 +0700

    Add UserModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { DatabaseModule } from './controllers/database/database.module';
 
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { entities } from './entities/entity.provider';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     TypeOrmModule.forFeature(entities),
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
   // constructor(private dataSourse: DataSource) {}ƒ
