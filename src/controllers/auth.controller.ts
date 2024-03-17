23ef0338cff7e9736572be4334638ebb3e213be0
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 00:29:36 +0700

    Add login and register endpoints to AuthController

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly _authService: AuthService) {}
+
+  @Post('login')
+  public async login() {
+    return this._authService.login();
+  }
+  @Post('register')
+  public async register(@Body() _userRegister: any): Promise<unknown> {
+    return this._authService.register(_userRegister);
+  }
 }
