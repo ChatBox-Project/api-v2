7e7f9ce117a58f2958e140c31af438d388e8e070
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 20:08:22 +0700

    Refactor auth.service.ts: Update register method and add createAccount method

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -2,45 +2,64 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AccountEntity } from 'src/entities/account.entity';
 import { AccountRepository } from 'src/repositories';
-import { UserRegisterDto, isValidEmail, isValidNumberPhone } from 'src/validators';
+import { UserRegisterDto, checkUsername } from 'src/validators';
 import { AccountService } from '../users/user.service';
-import { ErrorRespone } from 'src/errors';
+import { ErrorResponse } from 'src/errors';
+import { UserBaseEntity } from 'src/entities/user.base.entity';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(AccountEntity) _accountRepository: AccountRepository,
+    @InjectRepository(AccountEntity) private readonly _accountRepository: AccountRepository,
     private readonly _accountService: AccountService,
   ) {}
   public async login() {
     console.log('Login...');
   }
 
-  public async register(_userRegister: UserRegisterDto, _header: any): Promise<unknown> {
-    const { username, password, firstName, lastName, email } = _userRegister;
-
-    let data = null;
+  public async register(_accountRegister: UserRegisterDto, _header: any): Promise<unknown> {
+    try {
+      const data = await this._accountService.getAccountByUsername(_accountRegister.username);
+      
+      if (data) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Username is exist'),
+          errorCode: 'USERNAME_EXIST',
+        });
+      } else {
+        if (checkUsername(_accountRegister.username)) {
+          return this.createAccount(_accountRegister, true);
+        }
+      }
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException(error.message),
+        errorCode: 'REGISTER_FAIL',
+      });
+    }
 
     // check username is not exist
     // check before req register form app or web
 
-    if (_header?.isapp === 'true' || _header?.isapp === true) {
-      data = await this._accountService.getAccountByUsername(username);
-    }
-
-    if (data) {
-      throw new ErrorRespone({
-        ...new BadRequestException('Username is exist'),
-        errorCode: 'USERNAME_EXIST',
-      });
-    } else {
-      if (_header?.isapp === 'true' || _header?.isapp === true) {
-        
-      }
-    }
-
     return {};
   }
+  private async createAccount(_accountRegister: UserRegisterDto, isApp: boolean): Promise<boolean> {
+    try {
+      const { username, password, email } = _accountRegister;
+      const salt = await bcrypt.genSalt(10);
+      const hasdPassword = await bcrypt.hash(password, salt);
 
+      const newUser = await this._accountRepository.create({
+        ..._accountRegister,
+        password: hasdPassword,
+      });
 
+      console.log(newUser);
+      if (newUser) {
+      }
+    } catch (error) {}
+
+    return true;
+  }
 }
