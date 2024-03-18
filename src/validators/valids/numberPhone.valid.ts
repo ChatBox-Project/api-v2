eec03df1fe81c6cb65925441d6728e4e8bd3ecb4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 20:08:36 +0700

    Refactor numberPhone.valid.ts

--- src/validators/valids/numberPhone.valid.ts
+++ src/validators/valids/numberPhone.valid.ts
@@ -1,5 +1,3 @@
 export const isValidNumberPhone = (numberPhone: string): unknown => {
-  return /((0[3|5|7|8|9])+([0-9]{8})|([+]84[3|5|7|8|9])+([0-9]{8}))\b/g.test(
-    numberPhone,
-  );
+  return /((0[3|5|7|8|9])+([0-9]{8})|([+]84[3|5|7|8|9])+([0-9]{8}))\b/g.test(numberPhone);
 };
