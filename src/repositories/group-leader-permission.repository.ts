a6c1116e4dc331698ddd06b9fba77632835fd189
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:35 +0700

    Add GroupLeaderPermissionRepository class

---
+++ src/repositories/group-leader-permission.repository.ts
@@ -0,0 +1,4 @@
+import { BaseRepository } from './base.repository';
+import { GroupLeaderPermissionEntity } from '../entities/group-leader-permission.entity';
+
+export class GroupLeaderPermissionRepository extends BaseRepository<GroupLeaderPermissionEntity> {}
