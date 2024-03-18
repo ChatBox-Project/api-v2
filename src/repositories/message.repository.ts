05c57bf6bd677c1a12cb5a068bee230adaafd3b4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:38 +0700

    Add MessageRepository class

---
+++ src/repositories/message.repository.ts
@@ -0,0 +1,4 @@
+import { BaseRepository } from './base.repository';
+import { MessageEntity } from '../entities/message.entity';
+
+export class MessageRepository extends BaseRepository<MessageEntity> {}
