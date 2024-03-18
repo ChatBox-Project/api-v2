fb79b94c7f82787a1d8cc8e35b7020a7e7092791
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:28 +0700

    Add ChatBoxRepository class

---
+++ src/repositories/chat-box.entity.ts
@@ -0,0 +1,4 @@
+import { ChatBoxEntity } from 'src/entities/chat-box.entity';
+import { BaseRepository } from './base.repository';
+
+export class ChatBoxRepository extends BaseRepository<ChatBoxEntity> {}
