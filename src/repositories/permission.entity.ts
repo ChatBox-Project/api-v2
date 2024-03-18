e700222f813a02035c4e0fa0ff3f0a9dc6bbc998
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:42 +0700

    Add PermissionRepository class

---
+++ src/repositories/permission.entity.ts
@@ -0,0 +1,4 @@
+import { BaseRepository } from './base.repository';
+import { PermissionEntity } from '../entities/permission.entity';
+
+export class PermissionRepository extends BaseRepository<PermissionEntity> {}
