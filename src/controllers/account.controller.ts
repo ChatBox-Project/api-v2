fb9ae3d30b57cc8206594ad0f547e4e36d4855ee
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:07 +0700

    Add AccountController with login and register endpoints

---
+++ src/controllers/account.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { AuthService } from 'src/services/auth/auth.service';
+import { UserRegisterDto } from 'src/validators/dtos/auth/user-register.dto';
+
+@Controller('account')
+export class AccountController {
+  constructor(private readonly _authService: AuthService) {}
+
+  @Post('login')
+  public async login() {
+    return this._authService.login();
+  }
+  @Post('register')
+  public async register(
+    @Body() _userRegister: UserRegisterDto,
+    @Headers() _headers: any,
+  ): Promise<unknown> {
+    return this._authService.register(_userRegister, _headers);
+  }
+}
