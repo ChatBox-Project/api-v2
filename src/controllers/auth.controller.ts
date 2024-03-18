0b41ba8c5dc817eb196790be25e21c34f7b5675b
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 11:26:12 +0700

    Add UserRegisterDto to register method signature

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
+import { UserRegisterDto } from 'src/validators/dtos/auth/user-register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +11,9 @@ export class AuthController {
     return this._authService.login();
   }
   @Post('register')
-  public async register(@Body() _userRegister: any): Promise<unknown> {
+  public async register(
+    @Body() _userRegister: UserRegisterDto,
+  ): Promise<unknown> {
     return this._authService.register(_userRegister);
   }
 }
