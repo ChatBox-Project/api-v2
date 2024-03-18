16f7485350a241c37835979ba472d305107eb7de
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:31 +0700

    Add ChatGroupRepository class

---
+++ src/repositories/chat-group.repository.ts
@@ -0,0 +1,4 @@
+import { BaseRepository } from './base.repository';
+import { ChatGroupEntity } from '../entities/chat-group.entity';
+
+export class ChatGroupRepository extends BaseRepository<ChatGroupEntity> {}
