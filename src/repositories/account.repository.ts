76b7135c9c394574a696b2f3f4f3a62bcd89683c
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-03-18 13:05:21 +0700

    Add AccountRepository class

---
+++ src/repositories/account.repository.ts
@@ -0,0 +1,4 @@
+import { AccountEntity } from '../entities/account.entity';
+import { BaseRepository } from './base.repository';
+
+export class AccountRepository extends BaseRepository<AccountEntity> {}
