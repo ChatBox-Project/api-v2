7c9fc2401853d023ec8f6ed9bfc27bbf0ad6e4b0
daenv <daenvle69@gmail.com>
2024-03-29 23:40:22 +0700

    Refactor createTokenPair function to use async/await and handle errors

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -1,14 +1,29 @@
-import * as JWT from 'jsonwebtoken'
+import { BadRequestException } from '@nestjs/common';
+import * as JWT from 'jsonwebtoken';
+import { ErrorResponse } from 'src/errors';
 
-interface ITokenPair{
-          accessToken: string;
-          refreshToken: string;
+interface ITokenPair {
+  accessToken: string;
+  refreshToken: string;
 }
 
-export const createTokenPair = (payload: any, publicKey, privateKey) => {
-         try {
-                  // const accessToken = 
-         } catch (error) {
-                  
-         }
+export const createTokenPair = async (payload: any, publicKey, privateKey): Promise<ITokenPair> => {
+  try {
+    const accessToken = await JWT.sign(payload, privateKey, {
+      algorithm: 'RS256',
+      expiresIn: '2 days',
+    });
+    const refreshToken = await JWT.sign(payload, privateKey, {
+      algorithm: 'RS256',
+      expiresIn: '7 days',
+    });
+    await Promise.all([JWT.verify(accessToken, publicKey), JWT.verify(refreshToken, publicKey)]);
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ErrorResponse({
+      ...new BadRequestException(error.message),
+      errorCode: `Error creating token pair: ${error}`,
+    });
+  }
 };
