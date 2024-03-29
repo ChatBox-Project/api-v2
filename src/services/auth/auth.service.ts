66bf8318ad2102715621cc95531389e1d3e45909
daenv <daenvle69@gmail.com>
2024-03-29 21:39:49 +0700

    Fix bcrypt import and update account service import

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,10 +3,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AccountEntity } from 'src/entities/account.entity';
 import { AccountRepository } from 'src/repositories';
 import { UserRegisterDto, checkUsername } from 'src/validators';
-import { AccountService } from '../users/user.service';
+
 import { ErrorResponse } from 'src/errors';
 import { UserBaseEntity } from 'src/entities/user.base.entity';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
+import { AccountService } from '../account';
 
 @Injectable()
 export class AuthService {
@@ -20,29 +21,20 @@ export class AuthService {
 
   public async register(_accountRegister: UserRegisterDto, _header: any): Promise<unknown> {
     try {
-      const data = await this._accountService.getAccountByUsername(_accountRegister.username);
-
-      if (data) {
+      const holderAccount = await this._accountService.getAccountByUsername(_accountRegister.username);
+      if (holderAccount) {
         throw new ErrorResponse({
           ...new BadRequestException('Username is exist'),
           errorCode: 'USERNAME_EXIST',
         });
-      } else {
-        if (checkUsername(_accountRegister.username)) {
-          return this.createAccount(_accountRegister, true);
-        }
       }
+      return await this.createAccount(_accountRegister, _header.isApp);
     } catch (error) {
       throw new ErrorResponse({
         ...new BadRequestException(error.message),
         errorCode: 'REGISTER_FAIL',
       });
     }
-
-    // check username is not exist
-    // check before req register form app or web
-
-    return {};
   }
   private async createAccount(_accountRegister: UserRegisterDto, isApp: boolean): Promise<boolean> {
     try {
