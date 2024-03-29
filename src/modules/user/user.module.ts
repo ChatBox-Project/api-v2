6363c48d0d8a2e801b6479900b2fa9b7acb9b3eb
daenv <daenvle69@gmail.com>
2024-03-30 05:10:33 +0700

    Add TypeOrmModule and AuthModule imports to UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from 'src/controllers';
+import { entities } from 'src/entities/entity.provider';
 import { AuthService, UserService } from 'src/services';
+import { AuthModule } from '../auth';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature(entities), AuthModule],
   controllers: [UserController],
-  providers: [UserService, AuthService],
+  providers: [UserService],
 })
 export class UserModule {}
