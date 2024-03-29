4cabd216ba3a8024177ad1cff39658f933c87939
daenv <daenvle69@gmail.com>
2024-03-30 05:04:06 +0700

    Add create user endpoint to UserController

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,13 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
-
+import { UserService } from 'src/services';
+import { CreateUserDto } from 'src/validators';
 
 @Controller('users')
 export class UserController {
-         
+  constructor(private readonly _userService: UserService) {}
+
+  @Post('create')
+  public async create(@Body() user: CreateUserDto, @Headers() header: any): Promise<unknown> {
+    return this._userService.createUser(user, header);
+  }
 }
