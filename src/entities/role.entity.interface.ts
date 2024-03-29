d9704db621ea19d5c3dd7c15e8cafa3bf144c565
daenv <daenvle69@gmail.com>
2024-03-29 23:28:38 +0700

    Add role entity interface

---
+++ src/entities/role.entity.interface.ts
@@ -0,0 +1,10 @@
+import { RoleEnum } from 'src/configs/enums/role.enum';
+import { IBaseEntity } from './interfaces/base.entity.interface';
+import { AccountEntity } from './account.entity';
+
+export interface IRole extends IBaseEntity {
+  roleId: string;
+  roleName: RoleEnum;
+
+  account: AccountEntity;
+}
