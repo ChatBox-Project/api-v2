b843a3feb0588bd3a121e70ee767d51efd400926
daenv <daenvle69@gmail.com>
2024-03-29 21:39:35 +0700

    Add AccountService to AuthModule

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from 'src/controllers/auth.controller';
 import { entities } from 'src/entities/entity.provider';
+import { AccountService } from 'src/services/account/account.service';
 import { AuthService } from 'src/services/auth/auth.service';
-import { AccountService } from 'src/services/users/user.service';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
