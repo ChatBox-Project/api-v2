663b37dd052159e2a9de268868327acc45a6ee82
daenv <daenvle69@gmail.com>
2024-03-30 05:04:09 +0700

    Add UserRepository class

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,4 @@
+import { UserBaseEntity } from 'src/entities/user.base.entity';
+import { BaseRepository } from './base.repository';
+
+export class UserRepository extends BaseRepository<UserBaseEntity> {}
