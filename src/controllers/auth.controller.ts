075ed3a3beb2f552d2e4098ca7fb1532741415f4
daenv <daenvle69@gmail.com>
2024-03-31 17:40:06 +0700

    Add Joi validation pipe to register endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UsePipes } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
 import { UserRegisterDto } from 'src/validators/dtos/auth/user-register.dto';
+import { RegisterSchema } from 'src/validators/joi-schema/auth/register.joi.schema';
+import { JoiValidationPipe } from 'src/validators/pipes/joi';
 
 @Controller('auth')
 export class AuthController {
@@ -10,11 +12,9 @@ export class AuthController {
   public async login() {
     return this._authService.login();
   }
+  @UsePipes(new JoiValidationPipe(RegisterSchema))
   @Post('register')
-  public async register(
-    @Body() _userRegister: UserRegisterDto,
-    @Headers() _headers: any,
-  ): Promise<unknown> {
+  public async register(@Body() _userRegister: UserRegisterDto, @Headers() _headers: any): Promise<unknown> {
     return this._authService.register(_userRegister, _headers);
   }
 }
