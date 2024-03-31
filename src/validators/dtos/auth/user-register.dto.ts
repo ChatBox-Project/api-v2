e9c4384c67691009c42548de357932682cd0f56f
daenv <daenvle69@gmail.com>
2024-03-31 17:11:53 +0700

    Remove unnecessary validation decorators from UserRegisterDto

--- src/validators/dtos/auth/user-register.dto.ts
+++ src/validators/dtos/auth/user-register.dto.ts
@@ -1,17 +1,14 @@
 import { IsEmail, IsNotEmpty, IsPhoneNumber, MaxLength, MinLength } from 'class-validator';
 
 export class UserRegisterDto {
-  @IsNotEmpty()
   @MinLength(3)
   @MaxLength(50)
   username: string;
 
-  @IsNotEmpty()
   @MinLength(8)
   @MaxLength(50)
   password: string;
 
-  @IsNotEmpty()
   @MinLength(8)
   @MaxLength(50)
   passwordSalt: string;
