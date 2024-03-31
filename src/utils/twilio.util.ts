0961ba00a4eb22231fc6b4acf479bf338303ebf9
daenv <daenvle69@gmail.com>
2024-03-31 18:09:11 +0700

    Add Twilio utility function for sending SMS messages

---
+++ src/utils/twilio.util.ts
@@ -0,0 +1,19 @@
+import { Twilio } from 'twilio';
+import { constants } from '../constants';
+import { ErrorResponse } from 'src/errors';
+
+const { twilioAccountSID, twilioAuthToken, twilioPhoneNumber } = constants;
+const client = new Twilio(twilioAccountSID, twilioAuthToken);
+
+export const sendSMS = async (phoneNumber: string, message: string) => {
+  try {
+    const smsResponse = await client.messages.create({
+      from: twilioPhoneNumber,
+      to: phoneNumber,
+      body: message,
+    });
+    console.log('SMS Respones::',smsResponse.sid);
+  } catch (error) {
+    throw error;
+  }
+};
