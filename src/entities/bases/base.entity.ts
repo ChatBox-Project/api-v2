ef052a86ecd8d06d2bc16d6c518b87dbba5fcfaf
daenv <daenvle69@gmail.com>
2024-04-01 12:42:02 +0700

    Update BaseEntity column defaults

--- src/entities/bases/base.entity.ts
+++ src/entities/bases/base.entity.ts
@@ -3,30 +3,18 @@ import { IdEntity } from './id.entity';
 import { IBaseEntity } from '../interfaces/base.entity.interface';
 
 export abstract class BaseEntity extends IdEntity {
-  @Column({ type: 'boolean', default: true })
+  @Column({ type: 'boolean', default: false })
   isActive: boolean;
 
   @Column({ type: 'boolean', default: false })
   isArchieved: boolean;
 
-  @Column({ type: 'varchar', length: 300 })
+  @Column({ type: 'varchar', default: '' })
   createBy: string;
 
-  @Column({ type: 'varchar', length: 300 })
+  @Column({ type: 'varchar', default: '' })
   lastChangedBy: string;
 
-  @Column({ type: 'varchar', length: 300, nullable: true })
+  @Column({ type: 'varchar', default: '' })
   internalComment: string | null;
-
-  constructor(props?: IBaseEntity) {
-    super();
-    if (props) {
-      this.isActive = props.isActive;
-      this.isArchieved = props.isArchieved;
-      this.createBy = props.createBy;
-      this.lastChangedBy = props.lastChangedBy;
-      this.internalComment = props.internalComment;
-    }
-    Object.assign(this, props);
-  }
 }
