3a2775ae9451c553aba9211293e8117cc5e2b2a1
daenv <daenvle69@gmail.com>
2024-04-01 12:42:30 +0700

    Update UserEntity import in UserRepository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { UserBaseEntity } from 'src/entities/user.base.entity';
+import { UserEntity } from 'src/entities/user.base.entity';
 import { BaseRepository } from './base.repository';
 
-export class UserRepository extends BaseRepository<UserBaseEntity> {}
+export class UserRepository extends BaseRepository<UserEntity> {}
