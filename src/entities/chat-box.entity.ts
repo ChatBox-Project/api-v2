dad39e6925593d07336c180de1077d4b629e248e
daenv <daenvle69@gmail.com>
2024-04-01 12:41:15 +0700

    Refactor imports and fix entity reference in ChatBoxEntity

--- src/entities/chat-box.entity.ts
+++ src/entities/chat-box.entity.ts
@@ -1,15 +1,7 @@
 import { IChatBoxEntity } from './interfaces/chat-box.entity.interface';
 import { BaseEntity } from './bases/base.entity';
-import {
-  Column,
-  Entity,
-  Index,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { UserBaseEntity } from './user.base.entity';
+import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.base.entity';
 import { MessageEntity } from './message.entity';
 
 @Entity({ name: 'chat-box' })
@@ -23,21 +15,11 @@ export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {
   @Column({ name: 'chat_box_name', type: 'varchar', length: 255 })
   chatBoxName: string;
 
-  @OneToMany(() => UserBaseEntity, (user) => user.chatBox)
+  @OneToMany(() => UserEntity, (user) => user.chatBox)
   @JoinColumn()
-  user: UserBaseEntity;
+  user: UserEntity;
 
   @ManyToOne(() => MessageEntity, (mess) => mess.chatBox)
   @JoinColumn()
   message?: MessageEntity;
-
-  constructor(props?: ChatBoxEntity) {
-    super();
-    if (props) {
-      this.chatBoxId = props.chatBoxId;
-      this.chatBoxName = props.chatBoxName;
-      this.user = props.user;
-    }
-    Object.assign(this, props);
-  }
 }
