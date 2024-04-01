8f7c5eb7982635622592e8f20e25e77876a64114
daenv <daenvle69@gmail.com>
2024-04-01 12:42:16 +0700

    Update UserBaseEntity to UserEntity in group leader permission interface

--- src/entities/interfaces/group-leader-permission.entity.interface.ts
+++ src/entities/interfaces/group-leader-permission.entity.interface.ts
@@ -1,12 +1,12 @@
 import { ChatGroupEntity } from '../chat-group.entity';
 import { PermissionEntity } from '../permission.entity';
-import { UserBaseEntity } from '../user.base.entity';
+import { UserEntity } from '../user.base.entity';
 import { IBaseEntity } from './base.entity.interface';
 
 export interface IGroupLeaderPermission extends IBaseEntity {
   group_leader_permissionId: string;
-  leaderId: UserBaseEntity;
+  leaderId: UserEntity;
   group: ChatGroupEntity[] | string;
 
-  permission: PermissionEntity;
+  permission: PermissionEntity; // permissionId
 }
