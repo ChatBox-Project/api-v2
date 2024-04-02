e94f9576fd52f3c0133dde9e62d844d83c7a6a38
daenv <daenvle69@gmail.com>
2024-04-02 13:31:12 +0700

    Update auth.controller.ts with new imports and modified login and register methods

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, Headers, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UsePipes, Head } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
-import { UserRegisterDto } from 'src/validators/dtos/auth/user-register.dto';
+import {AccountLoginDto, AccountRegisterDto } from 'src/validators';
+
 import { RegisterSchema } from 'src/validators/joi-schema/auth/register.joi.schema';
 import { JoiValidationPipe } from 'src/validators/pipes/joi';
 import _ from 'underscore';
@@ -9,12 +10,13 @@ export class AuthController {
   constructor(private readonly _authService: AuthService) {}
 
   @Post('login')
-  public async login() {
-    return this._authService.login();
+  public async login(@Body() _req: AccountLoginDto, @Headers() _headers: any) {
+    const login = await this._authService.login(_req, _headers);
+    return _.omit(login, 'password');
   }
   @UsePipes(new JoiValidationPipe(RegisterSchema))
   @Post('register')
-  public async register(@Body() _userRegister: UserRegisterDto, @Headers() _headers: any): Promise<unknown> {
+  public async register(@Body() _userRegister: AccountRegisterDto, @Headers() _headers: any): Promise<unknown> {
     const register = await this._authService.register(_userRegister, _headers);
     return _.omit(register, 'password');
   }
