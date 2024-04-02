2188a981ab0df0e219e3e8b42bf0ea33b0d3a09f
daenv <daenvle69@gmail.com>
2024-04-02 16:13:03 +0700

    Refactor account controller to use new DTOs

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,20 +1,18 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
-import { UserRegisterDto } from 'src/validators/dtos/auth/account-register.dto';
+import { AccountLoginDto, AccountRegisterDto } from 'src/validators';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly _authService: AuthService) {}
 
-  @Post('login')
-  public async login() {
-    return this._authService.login();
-  }
+  // @Post('login')
+  // public async login(@Body() _accountLogin: AccountLoginDto, @Headers() _headers: any): Promise<unknown> {
+  //    this._authService.login();
+  //    return 
+  // }
   @Post('register')
-  public async register(
-    @Body() _userRegister: UserRegisterDto,
-    @Headers() _headers: any,
-  ): Promise<unknown> {
+  public async register(@Body() _userRegister: AccountRegisterDto, @Headers() _headers: any): Promise<unknown> {
     return this._authService.register(_userRegister, _headers);
   }
 }
