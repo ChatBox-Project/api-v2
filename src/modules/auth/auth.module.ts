f86c07214b2e0be31a855e3e3cda3c9bb31ef021
daenv <daenvle69@gmail.com>
2024-04-03 05:09:25 +0700

    Add OtpService to auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from 'src/controllers/auth.controller';
 import { entities } from 'src/entities/entity.provider';
-import { ResponseService } from 'src/services';
+import { OtpService, ResponseService } from 'src/services';
 import { AccountService } from 'src/services/account/account.service';
 import { AuthService } from 'src/services/auth/auth.service';
 import { KeyTokenService } from 'src/services/key/keyToken.service';
