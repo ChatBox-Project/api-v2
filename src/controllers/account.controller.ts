9f46b47bb4e9c126b65ee4de2cee2f1c62466b60
daenv <daenvle69@gmail.com>
2024-04-02 13:31:04 +0700

    Update UserRegisterDto import in AccountController

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from 'src/services/auth/auth.service';
-import { UserRegisterDto } from 'src/validators/dtos/auth/user-register.dto';
+import { UserRegisterDto } from 'src/validators/dtos/auth/account-register.dto';
 
 @Controller('account')
 export class AccountController {
