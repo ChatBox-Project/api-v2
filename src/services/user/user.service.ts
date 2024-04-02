5312fe4c4a9444f4217a837906d2954648a0549e
daenv <daenvle69@gmail.com>
2024-04-03 03:07:17 +0700

    Refactor user service to handle user creation and update

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,13 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AccountEntity } from 'src/entities/account.entity';
+import { ErrorResponse } from 'src/errors';
+import { AccountRepository, UserRepository } from 'src/repositories';
 import { CreateUserDto } from 'src/validators';
+import { ResponseService } from '../res';
+import { UserEntity } from 'src/entities/user.base.entity';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @InjectRepository(AccountEntity) private readonly _accountRepository: AccountRepository,
+    @InjectRepository(UserEntity) private readonly _userRepository: UserRepository,
+    private readonly _response: ResponseService,
+  ) {}
 
-  public async createUser(user: CreateUserDto, header: any) {
+  public async createUser(_userDto: CreateUserDto, _header: any) {
     try {
-      
-    } catch (error) {}
+      // check token
+      // console.log('params:: ', _header);
+      if (_header?.token === undefined) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Invalid token'),
+          errorCode: 'INVALID_TOKEN',
+        });
+      }
+      // console.log('header:: ', _header.token);
+
+      const foundAccount = await this._accountRepository.findOne({ where: { accessToken: _header.token } });
+      // console.log('user:: ', user)
+      if (!foundAccount) {
+        throw new ErrorResponse({
+          ...new BadRequestException('User not found'),
+          errorCode: 'USER_NOT_FOUND',
+        });
+      }
+
+      const existingUser = await this._userRepository.findOne({ where: { id: foundAccount.userId } });
+      console.log('existingUser:: ', existingUser);
+
+      if (existingUser) {
+        // Update existing user
+        await this._userRepository.update(existingUser.id, _userDto);
+      } else {
+        // Create a new user
+        const newUser = await this._userRepository.create({ ..._userDto });
+        await this._userRepository.save(newUser);
+
+        // Update the AccountEntity's user_id reference
+        await this._accountRepository.update(foundAccount.id, { userId: newUser.id });
+      }
+
+      const updatedAccount = await this._accountRepository.findOne({ where: { accessToken: _header.token } });
+      console.log('updatedAccount:: ', updatedAccount);
+
+      const metadata = { user: updatedAccount };
+      console.log('metadata:: ', metadata);
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException(error.message),
+        errorCode: 'USER_NOT_FOUND',
+      });
+    }
   }
 }
