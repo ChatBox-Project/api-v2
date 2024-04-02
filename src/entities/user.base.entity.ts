d362e71a08ac172593a5f28359427e98a82c0240
daenv <daenvle69@gmail.com>
2024-04-03 03:06:46 +0700

    Add primary generated column for user ID

--- src/entities/user.base.entity.ts
+++ src/entities/user.base.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToOne } from 'typeorm';
+import { Column, Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { IUserEntity } from './interfaces/user.base.entity.interface';
 import { EGender } from 'src/configs';
 import { BaseEntity } from './bases/base.entity';
@@ -10,6 +10,11 @@ import { GroupLeaderPermissionEntity } from './group-leader-permission.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity extends BaseEntity implements IUserEntity {
+  @Index('IX_USER_ID', { unique: true })
+  @Column({ name: 'user_id', type: 'uuid' })
+  @PrimaryGeneratedColumn()
+  id: string;
+
   @Column({ name: 'first_name', type: 'varchar', default: '' })
   @IsString()
   firstName: string;
@@ -28,24 +33,21 @@ export class UserEntity extends BaseEntity implements IUserEntity {
   @Column({ name: 'birth', type: 'date', default: new Date() })
   birth: Date;
 
-  @Column({ name: 'accound_id', type: 'uuid' })
-  @IsNotEmpty()
-  @OneToOne(() => AccountEntity)
-  @JoinColumn()
-  account: AccountEntity;
+  @OneToMany(() => AccountEntity, (account) => account.user)
+  accounts: AccountEntity[];
 
-  @Column({ name: 'chat_box_id', type: 'uuid' })
-  @ManyToOne(() => ChatBoxEntity, (chat) => chat.chatBoxId)
-  @JoinColumn()
-  chatBox?: ChatBoxEntity[];
+  // @Column({ name: 'chat_box_id', type: 'uuid' })
+  // @ManyToOne(() => ChatBoxEntity, (chat) => chat.chatBoxId)
+  // @JoinColumn()
+  // chatBox?: ChatBoxEntity[];
 
-  @Column({ name: 'chat_group_id', type: 'uuid' })
-  @ManyToMany(() => ChatGroupEntity)
-  @JoinTable()
-  chatGroup?: ChatGroupEntity[];
+  // @Column({ name: 'chat_group_id', type: 'uuid' })
+  // @ManyToMany(() => ChatGroupEntity)
+  // @JoinTable()
+  // chatGroup?: ChatGroupEntity[];
 
-  @Column({ name: 'group_leader_permission_id', type: 'uuid' })
-  @ManyToOne(() => GroupLeaderPermissionEntity, (gr) => gr.leaderId)
-  @JoinColumn()
-  groupLeaderPermission?: GroupLeaderPermissionEntity[];
+  // @Column({ name: 'group_leader_permission_id', type: 'uuid' })
+  // @ManyToOne(() => GroupLeaderPermissionEntity, (gr) => gr.leaderId)
+  // @JoinColumn()
+  // groupLeaderPermission?: GroupLeaderPermissionEntity[];
 }
