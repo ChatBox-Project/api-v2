ee7c94384161638339ad4fd2d703f3d15036d0bf
daenv <daenvle69@gmail.com>
2024-04-03 03:06:51 +0700

    Commented out user entity in chat box interface

--- src/entities/interfaces/chat-box.entity.interface.ts
+++ src/entities/interfaces/chat-box.entity.interface.ts
@@ -4,5 +4,5 @@ import { IBaseEntity } from './base.entity.interface';
 export interface IChatBoxEntity extends IBaseEntity {
   chatBoxId: string;
   chatBoxName: string;
-  user: UserEntity;
+  // user: UserEntity;
 }
