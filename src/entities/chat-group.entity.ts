ace4c8e55d5ee5822ddd37f205b454ab5b1b42d3
daenv <daenvle69@gmail.com>
2024-04-03 03:06:26 +0700

    Refactor ChatGroupEntity column types

--- src/entities/chat-group.entity.ts
+++ src/entities/chat-group.entity.ts
@@ -8,23 +8,23 @@ import { GroupLeaderPermissionEntity } from './group-leader-permission.entity';
 @Entity({ name: 'chat-group' })
 export class ChatGroupEntity extends BaseEntity implements IChatGroupEntity {
   @Index('IX_ChatGroup_GroupId', { unique: true })
-  @Column({ name: 'group_id', type: 'uuid', length: 255 })
+  @Column({ name: 'group_id', type: 'uuid' })
   @PrimaryGeneratedColumn()
   groupId: string;
 
   @Index('IX_ChatGroup_GroupName', { unique: true })
-  @Column({ name: 'group_name', type: 'varchar', length: 255 })
+  @Column({ name: 'group_name', type: 'varchar' })
   groupName: string;
 
-  @Column({ name: 'group_members', type: 'varchar', length: 255, array: true })
+  @Column({ name: 'group_members', type: 'varchar', array: true })
   groupMembers?: string[];
 
-  @Column({ name: 'group_leader_id', type: 'varchar', length: 255 })
+  @Column({ name: 'group_leader_id', type: 'varchar' })
   groupLeader: string;
 
-  @ManyToMany(() => UserEntity, (user) => user.chatGroup)
-  @JoinTable()
-  user: UserEntity[];
+  // @ManyToMany(() => UserEntity, (user) => user.chatGroup)
+  // @JoinTable()
+  // user: UserEntity[];
 
   @ManyToOne(() => GroupLeaderPermissionEntity, (gr) => gr.group)
   @JoinColumn()
