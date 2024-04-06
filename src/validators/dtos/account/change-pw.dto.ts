b6aa4a5568fd77b7f47e4122e5400dceab573c00
daenv <daenvle69@gmail.com>
2024-04-06 16:32:13 +0700

    Add changePwDto class for password validation

---
+++ src/validators/dtos/account/change-pw.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class changePwDto {
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(50)
+  @IsString()
+  pw: string;
+}
