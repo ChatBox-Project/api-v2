0937fb0ed4e6381b0425aeb8af6f49bcab52f7ab
daenv <daenvle69@gmail.com>
2024-04-06 16:31:51 +0700

    Refactor account controller with new endpoints

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,18 +1,20 @@
-import { Body, Controller, Post, Headers } from '@nestjs/common';
-import { AuthService } from 'src/services/auth/auth.service';
-import { AccountLoginDto, AccountRegisterDto } from 'src/validators';
-
+import { Body, Controller, Post, Headers, Put } from '@nestjs/common';
+import { AccountService } from 'src/services';
+import { changePwDto, ForgotPwDto } from 'src/validators';
+import _ from 'underscore';
 @Controller('account')
 export class AccountController {
-  constructor(private readonly _authService: AuthService) {}
+  constructor(private readonly _accountSerivce: AccountService) {}
 
-  // @Post('login')
-  // public async login(@Body() _accountLogin: AccountLoginDto, @Headers() _headers: any): Promise<unknown> {
-  //    this._authService.login();
-  //    return 
-  // }
-  @Post('register')
-  public async register(@Body() _userRegister: AccountRegisterDto, @Headers() _headers: any): Promise<unknown> {
-    return this._authService.register(_userRegister, _headers);
+  @Put('changepw')
+  public async changePassword(@Headers() _headers: any, @Body() pw: changePwDto): Promise<unknown> {
+    const changePw = await this._accountSerivce.changePassword(_headers, pw);
+    return _.omit(changePw, 'password');
+  }
+
+  @Put('forgotpw')
+  public async forgotPassword(@Body() phoneNumber: ForgotPwDto): Promise<unknown> {
+    const forgotPw = await this._accountSerivce.forgotPassword(phoneNumber);
+    return _.omit(forgotPw, 'password');
   }
 }
