fa241391c10e393c1fd48fb61c57b2302345bdaa
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 17:51:43 +0700

    Refactor account.controller.ts: Add GET method for retrieving account information

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Headers, Put, Param, Patch, Query } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Put, Param, Patch, Query, Get } from '@nestjs/common';
 import { AccountService } from 'src/services';
 import { ChangePwDto, ForgotPwDto } from 'src/validators';
 import _ from 'underscore';
@@ -8,7 +8,7 @@ export class AccountController {
   // after login
   @Put('changepw')
   public async changePassword(@Headers() _headers: any, @Body() pw: ChangePwDto): Promise<unknown> {
-    const changePw = await this._accountSerivce.changePassword(_headers, pw);
+    const changePw = await this._accountSerivce.changePassword(_headers.token, pw);
     return _.omit(changePw, 'password');
   }
 
@@ -17,5 +17,10 @@ export class AccountController {
   public async forgotPassword(@Query() _phone: ForgotPwDto, @Body() pw: ChangePwDto): Promise<unknown> {
     const forgotPw = await this._accountSerivce.forgotPassword(_phone, pw);
     return _.omit(forgotPw, 'password');
+  }
+  @Get()
+  public async getAccount(@Headers() _header: any): Promise<unknown> {
+    const account = await this._accountSerivce.getAccount(_header.token);
+    return _.omit(account);
   }
 }
