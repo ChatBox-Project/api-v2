997bcc7236cfb36fab35495f2e77b4a42e91c0f0
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 22:28:08 +0700

    Add Swagger documentation for AccountRegisterDto in account API

--- src/validators/dtos/auth/account-register.dto.ts
+++ src/validators/dtos/auth/account-register.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class AccountRegisterDto {
   @IsNotEmpty()
   @MaxLength(10)
   @MinLength(10)
+  @ApiProperty()
   phoneNumber: string;
 
+  @ApiProperty()
   @MinLength(8)
   @MaxLength(50)
   password: string;
