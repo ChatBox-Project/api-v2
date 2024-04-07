3c6bc47ecc502915181bd89be6a8a48a2cac6e17
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 22:27:50 +0700

    Add Swagger documentation for Chat Product API

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
   app.enableCors();
   app.use(passport.initialize());
 
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Chat Product Api')
+    .setDescription('Chat Product Api description')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
