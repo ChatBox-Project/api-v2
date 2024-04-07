966a50939045244e401190fbd1517645caa6c5a2
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 11:59:22 +0700

    Add listFriend property to UserEntity

--- src/entities/interfaces/user.base.entity.interface.ts
+++ src/entities/interfaces/user.base.entity.interface.ts
@@ -4,12 +4,14 @@ import { AccountEntity } from '../account.entity';
 import { ChatBoxEntity } from '../chat-box.entity';
 import { ChatGroupEntity } from '../chat-group.entity';
 import { GroupLeaderPermissionEntity } from '../group-leader-permission.entity';
+import { IListFriend } from './listFriend.interface';
 
 export interface IUserEntity extends IBaseEntity {
   name: string;
   gender: EGender;
   avatarUrl: string;
   birth: Date;
+  listFriend: IListFriend[];
   // chatBox?: ChatBoxEntity[];
   // chatGroup?: ChatGroupEntity[];
   // groupLeaderPermission?: GroupLeaderPermissionEntity[];
