baef67ff798399bbf1c140c52339335b3691377a
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 23:05:03 +0700

    Add Swagger documentation for UpdateUserDto in user API

--- src/validators/dtos/user/update-user.dto.ts
+++ src/validators/dtos/user/update-user.dto.ts
@@ -1,10 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { EGender } from 'src/configs';
 
 export class UpdateUserDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   gender: EGender;
   // @IsUrl()
+  @ApiProperty()
   avatarUrl: string;
   // @IsDate()
+  @ApiProperty()
   birth: Date;
 }
