2c8bd83b140dcdd63ceaf390856fbe791737340a
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 17:09:41 +0700

    Refactor account.controller.ts: Update forgotPassword method to use PATCH instead of PUT

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,22 +1,21 @@
-import { Body, Controller, Post, Headers, Put, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Put, Param, Patch, Query } from '@nestjs/common';
 import { AccountService } from 'src/services';
-import { changePwDto, ForgotPwDto } from 'src/validators';
+import { ChangePwDto, ForgotPwDto } from 'src/validators';
 import _ from 'underscore';
 @Controller('account')
 export class AccountController {
   constructor(private readonly _accountSerivce: AccountService) {}
   // after login
   @Put('changepw')
-  public async changePassword(@Headers() _headers: any, @Body() pw: changePwDto): Promise<unknown> {
+  public async changePassword(@Headers() _headers: any, @Body() pw: ChangePwDto): Promise<unknown> {
     const changePw = await this._accountSerivce.changePassword(_headers, pw);
     return _.omit(changePw, 'password');
   }
 
   // before login
-  @Patch('forgotpw')
-  public async forgotPassword(@Headers() _headers: any, @Body() pw: ForgotPwDto) {
-    console.log(_headers.phone, pw.phoneNumber);
-    // const forgotPw = await this._accountSerivce.forgotPassword(_headers.phone, pw.phoneNumber);
-    // return _.omit(forgotPw, 'password');
+  @Put('forgotpw')
+  public async forgotPassword(@Query() _phone: ForgotPwDto, @Body() pw: ChangePwDto): Promise<unknown> {
+    const forgotPw = await this._accountSerivce.forgotPassword(_phone, pw);
+    return _.omit(forgotPw, 'password');
   }
 }
