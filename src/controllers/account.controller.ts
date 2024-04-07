ff7a6c6581688f3dff0cbaf7a33678327a11791d
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-07 10:20:48 +0700

    Refactor changePassword and forgotPassword methods in AccountController

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -5,13 +5,14 @@ import _ from 'underscore';
 @Controller('account')
 export class AccountController {
   constructor(private readonly _accountSerivce: AccountService) {}
-
+  // after login
   @Put('changepw')
   public async changePassword(@Headers() _headers: any, @Body() pw: changePwDto): Promise<unknown> {
     const changePw = await this._accountSerivce.changePassword(_headers, pw);
-    return _.omit(changePw, 'password');
+    return _.omit(changePw, 'password'); 
   }
-
+  
+  // before login
   @Put('forgotpw')
   public async forgotPassword(@Param() phoneNumber: ForgotPwDto, @Body() pw: changePwDto): Promise<unknown> {
     const forgotPw = await this._accountSerivce.forgotPassword(phoneNumber, pw);
