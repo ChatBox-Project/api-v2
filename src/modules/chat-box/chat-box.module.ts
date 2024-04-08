3097022eb0b6af6477fadd90d19baea14f8b1e8f
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-08 21:31:29 +0700

    Update ChatBoxModule to import ResponseService and UserRepository

--- src/modules/chat-box/chat-box.module.ts
+++ src/modules/chat-box/chat-box.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatBoxController } from 'src/controllers/';
+import { ChatBoxService, ResponseService } from 'src/services/';
 import { entities } from 'src/entities/entity.provider';
-import { ChatBoxService } from 'src/services/';
+import { UserRepository } from 'src/repositories';
 
 @Module({
-  imports: [TypeOrmModule.forFeature(entities)],
-  providers: [ChatBoxService],
-controllers:[ChatBoxController]
+  imports: [TypeOrmModule.forFeature([...entities])],
+  providers: [ChatBoxService, ResponseService],
+  controllers: [ChatBoxController],
 })
 export class ChatBoxModule {}
