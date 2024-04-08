d9a65b090243be92691494ba09c37ee2a5e891c9
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-08 23:14:58 +0700

    Add chatBoxService property to AppGateWay in chat.gateway.service.ts

--- src/services/gateway/chat.gateway.service.ts
+++ src/services/gateway/chat.gateway.service.ts
@@ -6,6 +6,7 @@ import { MessageService } from '../messages';
 @Injectable()
 export class AppGateWay implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private logger: Logger = new Logger('AppGateway');
+  chatBoxService: any;
   constructor(private messageService: MessageService) {}
 
   @WebSocketServer() server: Server;
@@ -14,6 +15,48 @@ export class AppGateWay implements OnGatewayInit, OnGatewayConnection, OnGateway
   public async handleSendMessage(client: Socket, payload: MessageEntity): Promise<void> {
     await this.messageService.saveMessage(payload);
     this.server.emit('sendMessage', payload);
+  }
+
+  @SubscribeMessage('createChatBox')
+  public async handleCreateChatBox(client: Socket, data: { token: string; _id: string }): Promise<void> {
+    try {
+      const { token, _id } = data;
+      const res = await this.chatBoxService.createChatBox(token, _id);
+      client.emit('createChatBox', res);
+    } catch (error) {
+      client.emit('error', error);
+    }
+  }
+
+  @SubscribeMessage('getChatBox')
+  public async handleGetChatBox(client: Socket, token: string): Promise<void> {
+    try {
+      const res = await this.chatBoxService.getChatBox(token);
+      client.emit('getChatBox', res);
+    } catch (error) {
+      client.emit('error', error);
+    }
+  }
+
+  // @SubscribeMessage('getChatBoxById')
+  // public async handleGetChatBoxById(client: Socket, _id: string): Promise<void> {
+  //   try {
+  //     const res = await this.chatBoxService.getChatBoxById(_id);
+  //     client.emit('getChatBoxById', res);
+  //   } catch (error) {
+  //     client.emit('error', error);
+  //   }
+  // }
+
+  @SubscribeMessage('deleteChatBox')
+  public async handleDeleteChatBox(client: Socket, data: { token: string; _id: string }): Promise<void> {
+    try {
+      const { token, _id } = data;
+      const res = await this.chatBoxService.deleteChatBox(token, _id);
+      client.emit('deleteChatBox', res);
+    } catch (error) {
+      client.emit('error', error);
+    }
   }
 
   private users: number = 0;
