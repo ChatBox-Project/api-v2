94b3747fe82907de7291dbad5e8b4b33d9c6cd3c
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-08 17:02:54 +0700

    Add createChatBox method to handle chat box creation in ChatBoxController

--- src/controllers/chat-box.controller.ts
+++ src/controllers/chat-box.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UsePipes, Param, Query } from '@nestjs/common';
+import { ChatBoxService } from 'src/services';
+import _ from 'underscore';
 
 @Controller('chat')
-export class ChatBoxController {}
+export class ChatBoxController {
+  constructor(private readonly _chatBoxService: ChatBoxService) {}
+
+  // crud
+  @Post(':id')
+  public async createChatBox(@Headers('token') token: string, @Query('id') id: string) {
+    const create = await this._chatBoxService.createChatBox(token, id);
+    return _.omit(create, 'chatbox');
+  }
+}
