25b2b76b01375d63a6ce38ad060c2a1ef108c328
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-08 16:05:26 +0700

    Remove unused groupLeaderPermission property from permission.entity.interface.ts

--- src/entities/interfaces/permission.entity.interface.ts
+++ src/entities/interfaces/permission.entity.interface.ts
@@ -1,7 +1,5 @@
-import { GroupLeaderPermissionEntity } from '../group-leader-permission.entity';
 import { IBaseEntity } from './base.entity.interface';
 
 export interface IPermission extends IBaseEntity {
   name: string;
-  groupLeaderPermission: GroupLeaderPermissionEntity[];
 }
