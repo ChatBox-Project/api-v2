eef4955415fac04b2e6ed47e48fe1954eb7c7375
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-08 22:31:36 +0700

    Add CRUD operations for ChatBoxController

--- src/controllers/chat-box.controller.ts
+++ src/controllers/chat-box.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, Headers, UsePipes, Param, Query } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UsePipes, Param, Query, Get, Delete } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { ChatBoxService } from 'src/services';
 import _ from 'underscore';
 
@@ -8,8 +9,28 @@ export class ChatBoxController {
 
   // crud
   @Post(':id')
+  @ApiOkResponse({ description: 'Create chat box' })
   public async createChatBox(@Headers('token') token: string, @Param('id') id: string) {
     const create = await this._chatBoxService.createChatBox(token, id);
     return _.omit(create, 'chatbox');
+  }
+  @Get()
+  @ApiOkResponse({ description: 'Gets chat box' })
+  public async getChatBoxs(@Headers('token') token: string) {
+    const chatBox = await this._chatBoxService.getChatBox(token);
+    return _.omit(chatBox, 'chatbox');
+  }
+  @ApiOkResponse({ description: 'Delete chat box' })
+  @Delete(':id')
+  public async deleteChatBox(@Headers('token') token: string, @Param('id') _id: string) {
+    const deleteChatBox = await this._chatBoxService.deleteChatBox(token, _id);
+    return _.omit(deleteChatBox, 'chatbox');
+  }
+
+  @ApiOkResponse({ description: 'Get chat box by id' })
+  @Get(':id')
+  public async getChatBoxById(@Param('id') _id: string) {
+    const chatBox = await this._chatBoxService.getChatBoxById(_id);
+    return _.omit(chatBox, 'chatbox');
   }
 }
