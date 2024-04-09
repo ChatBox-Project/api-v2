fc07c0ee29018e033b16c4e71fb14499382efc77
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-09 10:25:23 +0700

    Add ResponseService to MessageModule providers

--- src/modules/messages/message.module.ts
+++ src/modules/messages/message.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageController } from 'src/controllers';
 import { entities } from 'src/entities/entity.provider';
-import { AppGateWay, MessageService } from 'src/services';
+import { AppGateWay, MessageService, ResponseService } from 'src/services';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
   controllers: [MessageController],
-  providers: [MessageService, AppGateWay],
+  providers: [MessageService, AppGateWay, ResponseService],
 })
 export class MessageModule {}
