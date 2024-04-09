b0dc4098c207226780c018a204338a6b68b9d139
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-09 10:25:09 +0700

    Refactor ChatBoxEntity to add sender_id and receiver_id properties

--- src/entities/chat-box.entity.ts
+++ src/entities/chat-box.entity.ts
@@ -1,12 +1,10 @@
-import { IChatBoxEntity } from './interfaces/chat-box.entity.interface';
+import { Column, Entity, Index, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 import { BaseEntity } from './bases/base.entity';
-import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { UserEntity } from './user.base.entity';
 import { MessageEntity } from './message.entity';
+import { UserEntity } from './user.base.entity';
 
 @Entity({ name: 'chat-box' })
-export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {
-
+export class ChatBoxEntity extends BaseEntity {
   @Column({ name: 'chat_box_name', type: 'varchar' })
   chatBoxName: string;
 
@@ -18,9 +16,10 @@ export class ChatBoxEntity extends BaseEntity implements IChatBoxEntity {
   @Column({ name: 'receiver_id', type: 'uuid' })
   receiver_id: string;
 
-  @ManyToOne(() => UserEntity)
+  @ManyToMany(() => UserEntity)
   user: UserEntity[];
 
-  @ManyToOne(() => MessageEntity, (mess) => mess.chatBox)
-  message?: MessageEntity[] | MessageEntity;
+  @JoinTable()
+  @OneToMany(() => MessageEntity, (mess) => mess.chatBox)
+  message: MessageEntity[];
 }
