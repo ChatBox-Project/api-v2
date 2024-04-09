052678da1271dfcd6a46d47de01b2513fb5cca35
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-09 10:25:18 +0700

    Refactor IMessage interface to include contentMessage property

--- src/entities/interfaces/message.entity.interface.ts
+++ src/entities/interfaces/message.entity.interface.ts
@@ -3,11 +3,6 @@ import { IBaseEntity } from './base.entity.interface';
 export interface IMessage extends IBaseEntity {
   messageId: string;
   senderId: string;
-  messageType?: string;
-  contentText?: string;
-  contentImage?: string;
-  contentAudio?: string;
-  contentVideo?: string;
-  contentFile?: string;
-
+  contentMessage: string;
+  messageType: string;
 }
