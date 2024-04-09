685850b2fef1ed6fa21453e586014fc99a35dd08
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-09 10:25:29 +0700

    Refactor createMessage method in MessageService to use ChatBoxEntity and ResponseService

--- src/services/messages/message.service.ts
+++ src/services/messages/message.service.ts
@@ -1,30 +1,44 @@
 import { BadRequestException, Body, Headers, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ChatBoxEntity } from 'src/entities';
 import { AccountEntity } from 'src/entities/account.entity';
 import { MessageEntity } from 'src/entities/message.entity';
 import { UserEntity } from 'src/entities/user.base.entity';
 import { ErrorResponse } from 'src/errors';
-import { AccountRepository, MessageRepository, UserRepository } from 'src/repositories';
+import { AccountRepository, ChatBoxRepository, MessageRepository, UserRepository } from 'src/repositories';
 import { CreateMessageDto } from 'src/validators';
+import { ResponseService } from '../res';
 
 @Injectable()
 export class MessageService {
   constructor(
     @InjectRepository(MessageEntity) private readonly _messageRepository: MessageRepository,
-    @InjectRepository(AccountEntity) private readonly _accountRepository: AccountRepository,
+    @InjectRepository(ChatBoxEntity) private readonly _chatBoxRepository: ChatBoxRepository,
     @InjectRepository(UserEntity) private readonly _userRepository: UserRepository,
+    private readonly _response: ResponseService,
   ) {}
 
-  public async createMessage(token: string, message: CreateMessageDto) {
+  public async createMessage(_id: string, message: CreateMessageDto): Promise<unknown> {
     try {
-      // find token
-      const holderAccount = await this.findAccountByToken(token);
-      console.log('holderAccount:: ', holderAccount);
-      // find user
-      const holderUser = await this.findUserByAccountId(holderAccount.userId);
-      console.log('holderUser:: ', holderUser);
+      // check id
+      const chatbox = await this._chatBoxRepository.findOneOrFail({ where: { id: _id } });
+      // console.log('checkIDChatbox:: ', checkIDChatbox);
+      const saveMessage = await this._messageRepository.create({
+        ...message,
+        senderId: chatbox.sender_id,
+        receiverId: chatbox.receiver_id,
+        chatBox: chatbox,
+      });
+      await this._messageRepository.save(saveMessage);
 
+      // Update user's chat box
+      chatbox.message = [...(chatbox.message || []), saveMessage];
+      const test = await this._chatBoxRepository.save(chatbox);  
+      console.log('test:: ', test);
 
+      const metadata = { message: saveMessage };
+      const res = this._response.createResponse(200, 'Create message success', metadata);
+      return res;
     } catch (error) {
       throw new ErrorResponse({
         ...new BadRequestException(error.message),
@@ -35,52 +49,5 @@ export class MessageService {
   public async saveMessage(message: MessageEntity) {}
   public async getMessages(): Promise<MessageEntity[]> {
     return await this._messageRepository.find();
-  }
-
-  private async findMessageById(id: string): Promise<MessageEntity> {
-    //check if the message
-    const message = await this._messageRepository.findOne({ where: { messageId: id } });
-    if (!message) {
-      throw new ErrorResponse({
-        ...new BadRequestException('Message not found'),
-        errorCode: 'MESSAGE_NOT_FOUND',
-      });
-    }
-    return message;
-  }
-  private async findAccountByToken(token: string): Promise<AccountEntity> {
-    // Check token
-    if (!token) {
-      throw new ErrorResponse({
-        ...new BadRequestException('Invalid token'),
-        errorCode: 'INVALID_TOKEN',
-      });
-    }
-    const account = await this._accountRepository.findOne({ where: { accessToken: token } });
-    if (!account) {
-      throw new ErrorResponse({
-        ...new BadRequestException('User not found'),
-        errorCode: 'USER_NOT_FOUND',
-      });
-    }
-    return account;
-  }
-
-  private async findUserByAccountId(accountId: string): Promise<UserEntity> {
-    // Check token
-    if (!accountId) {
-      throw new ErrorResponse({
-        ...new BadRequestException('Invalid token'),
-        errorCode: 'INVALID_TOKEN',
-      });
-    }
-    const user = await this._userRepository.findOne({ where: { id: accountId } });
-    if (!user) {
-      throw new ErrorResponse({
-        ...new BadRequestException('User not found'),
-        errorCode: 'USER_NOT_FOUND',
-      });
-    }
-    return user;
   }
 }
