5dbd8b8cca7ab41c7e5230866e0d26ccccc8a24f
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-10 08:36:03 +0700

    Refactor MessageModule to import and use additional repositories

--- src/modules/messages/message.module.ts
+++ src/modules/messages/message.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageController } from 'src/controllers';
 import { entities } from 'src/entities/entity.provider';
+import { AccountRepository, ChatBoxRepository, MessageRepository, UserRepository } from 'src/repositories';
 import { AppGateWay, MessageService, ResponseService } from 'src/services';
+// import { AppGateWayModule } from '../gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
   controllers: [MessageController],
-  providers: [MessageService, AppGateWay, ResponseService],
+  providers: [MessageService, MessageRepository, ChatBoxRepository, UserRepository, AccountRepository, ResponseService, AppGateWay],
 })
 export class MessageModule {}
