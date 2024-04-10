61e05f1ed0b4f295a0a27744f5666d17a000a5dd
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-10 08:36:28 +0700

    Fix missing indentation in AccountRegisterDto class

--- src/validators/dtos/auth/account-register.dto.ts
+++ src/validators/dtos/auth/account-register.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class AccountRegisterDto {
+  
   @IsNotEmpty()
   @MaxLength(10)
   @MinLength(10)
