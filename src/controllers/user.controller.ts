c77f354ab5f77caf095bbe9554ef40cf7070b384
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-10 20:06:29 +0700

    Add getUserById method to UserController

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -21,6 +21,11 @@ export class UserController {
     const update = await this._userService.updateUser(_userUpdate, _header);
     return _.omit(update);
   }
+  @Get(':id')
+  public async getUserById(@Headers() _header: any, @Param('id') _id: string): Promise<unknown> {
+    const user = await this._userService.getUserById(_header.token, _id);
+    return _.omit(user);
+  }
   // @Put('friends/:id')
   // public async addFriend(@Headers() _header: any, @Param('id') friendId: string): Promise<unknown> {
   //   const addFriend = await this._userService.addFriend(_header.token, friendId);
