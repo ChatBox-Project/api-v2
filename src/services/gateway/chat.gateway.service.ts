37bae92dd28ed734ce7f830eb549fdc453605e97
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-10 08:36:21 +0700

    Refactor MessageModule to import and use additional repositories Refactor index.ts to export the new chat-box module Refactor order of messages in MessageService to display the most recent messages first Refactor MessageService to remove console.log statement and unused chatboxId property Refactor getChatboxMessages method in MessageService to handle messages based on the holder user Refactor MessageService to remove console.log statement and unused chatboxId property Refactor MessageEntity to remove unused chatboxId property Refactor ChatBoxEntity to remove chatBoxId column and update column names Refactor column type in ChatBoxEntity and MessageEntity to remove explicit type definition Refactor order of messages in MessageService to use ascending order of lastChangedDateTime

--- src/services/gateway/chat.gateway.service.ts
+++ src/services/gateway/chat.gateway.service.ts
@@ -1,79 +1,154 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageEntity } from 'src/entities/message.entity';
 import { MessageService } from '../messages';
+import { ChatBoxService } from '../chat-box';
+import { AccountEntity, ChatBoxEntity, UserEntity } from 'src/entities';
+import { ErrorResponse } from 'src/errors';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AccountRegisterDto } from 'src/validators';
+import { AccountRepository, ChatBoxRepository, UserRepository } from 'src/repositories';
 @Injectable()
 export class AppGateWay implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private logger: Logger = new Logger('AppGateway');
-  chatBoxService: any;
-  constructor(private messageService: MessageService) {}
+
+  constructor(
+    private messageService: MessageService,
+    @InjectRepository(AccountEntity) private readonly _accountRepository: AccountRepository,
+    @InjectRepository(UserEntity) private readonly _userRepository: UserRepository,
+    @InjectRepository(ChatBoxEntity) private readonly _chatBoxRepository: ChatBoxRepository,
+  ) {}
 
   @WebSocketServer() server: Server;
 
-  // @SubscribeMessage('sendMessage')
-  // public async handleSendMessage(client: Socket, payload: MessageEntity): Promise<void> {
-  //   await this.messageService.saveMessage(payload);
-  //   this.server.emit('sendMessage', payload);
-  // }
-
-  @SubscribeMessage('createChatBox')
-  public async handleCreateChatBox(client: Socket, data: { token: string; _id: string }): Promise<void> {
+  @SubscribeMessage('sendMessage')
+  public async handleMessage(client: Socket, payload: any): Promise<unknown> {
     try {
-      const { token, _id } = data;
-      const res = await this.chatBoxService.createChatBox(token, _id);
-      client.emit('createChatBox', res);
+      const { token, id, data } = payload;
+
+      const sender = await this.authenticateUser(token);
+
+      const message = await this.messageService.createMessage(token, id, data);
+
+      client.broadcast.to(id).emit('newMessage', message);
+      return message;
     } catch (error) {
-      client.emit('error', error);
+      throw new Error('Failed to send message to chat box.');
     }
   }
 
-  @SubscribeMessage('getChatBox')
-  public async handleGetChatBox(client: Socket, token: string): Promise<void> {
+  @SubscribeMessage('getMessages')
+  public async handleGetMessages(client: Socket, payload: any): Promise<unknown> {
     try {
-      const res = await this.chatBoxService.getChatBox(token);
-      client.emit('getChatBox', res);
+      const { token, id } = payload;
+
+      const sender = await this.authenticateUser(token);
+
+      const messages = await this.messageService.getChatboxMessages(token, id);
+
+      client.broadcast.to(id).emit('getMessages', messages);
+      return messages;
     } catch (error) {
-      client.emit('error', error);
+      throw new Error('Failed to get messages from chat box.');
     }
   }
 
-  // @SubscribeMessage('getChatBoxById')
-  // public async handleGetChatBoxById(client: Socket, _id: string): Promise<void> {
-  //   try {
-  //     const res = await this.chatBoxService.getChatBoxById(_id);
-  //     client.emit('getChatBoxById', res);
-  //   } catch (error) {
-  //     client.emit('error', error);
-  //   }
-  // }
-
-  @SubscribeMessage('deleteChatBox')
-  public async handleDeleteChatBox(client: Socket, data: { token: string; _id: string }): Promise<void> {
+  @SubscribeMessage('getMessage')
+  public async handleGetMessage(client: Socket, payload: any): Promise<unknown> {
     try {
-      const { token, _id } = data;
-      const res = await this.chatBoxService.deleteChatBox(token, _id);
-      client.emit('deleteChatBox', res);
+      const { token, id, messageId } = payload;
+
+      const sender = await this.authenticateUser(token);
+
+      const message = await this.messageService.getMessageById(token, id, messageId);
+
+      client.broadcast.to(id).emit('getMessage', message);
+      return message;
     } catch (error) {
-      client.emit('error', error);
+      throw new Error('Failed to get message from chat box.');
     }
   }
 
-  private users: number = 0;
+  @SubscribeMessage('updateMessage')
+  public async handleUpdateMessage(client: Socket, payload: any): Promise<MessageEntity> {
+    try {
+      const { token, _id, messageId, data } = payload;
 
-  afterInit(server: Server) {
-    this.logger.log('Init');
-  }
+      // Kiểm tra xác thực người dùng
+      await this.authenticateUser(token);
 
-  handleConnection(client: Socket, ...args: any[]) {
-    this.users++;
-    this.logger.log(`Client connected: ${client.id}`);
-    this.server.emit('users', this.users);
+      // Kiểm tra hộp chat
+      await this.findBoxChat(_id);
+
+      // Lấy tin nhắn từ cơ sở dữ liệu
+      const message = await this.messageService.updateMessage(token, _id, messageId, data);
+
+      // Gửi tin nhắn đã cập nhật tới tất cả các client trong hộp chat
+      client.broadcast.to(_id).emit('updatedMessage', message);
+
+      return message;
+    } catch (error) {
+      throw new Error('Failed to update message.');
+    }
   }
 
   handleDisconnect(client: Socket) {
-    this.users--;
-    this.logger.log(`Client disconnected: ${client.id}`);
-    this.server.emit('users', this.users);
+    this.logger.log(`Client ${client.id} disconnected`);
+  }
+  handleConnection(client: Socket) {
+    this.logger.log(`Client ${client.id} connected`);
+  }
+  afterInit(server: Server) {
+    this.logger.log('Gateway initialized');
+  }
+
+  private async authenticateUser(token: string): Promise<UserEntity> {
+    try {
+      if (!token) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Token is required'),
+          errorCode: 'TOKEN_REQUIRED',
+        });
+      }
+      const account = await this._accountRepository.findOne({ where: { accessToken: token } });
+      if (!account) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Account is not exist'),
+          errorCode: 'ACCOUNT_NOT_EXIST',
+        });
+      }
+      // console.log('_id:: ', _id)
+      const user = await this._userRepository.findOne({ where: { id: account.userId } });
+      if (!user) {
+        throw new ErrorResponse({
+          ...new BadRequestException('User is not exist'),
+          errorCode: 'USER_NOT_EXIST',
+        });
+      }
+      return user;
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException(error.message),
+        errorCode: 'FIND_USER_BY_ID_ERROR',
+      });
+    }
+  }
+  private async findBoxChat(_id: string): Promise<ChatBoxEntity> {
+    try {
+      // check id
+      const chatbox = await this._chatBoxRepository.findOneOrFail({ where: { id: _id } });
+      if (!chatbox)
+        throw new ErrorResponse({
+          ...new BadRequestException('Chat box is not exist'),
+          errorCode: 'BoxChat_NOT_FOUND',
+        });
+      return chatbox;
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException(error.message),
+        errorCode: 'BoxChat_NOT_FOUND',
+      });
+    }
   }
 }
