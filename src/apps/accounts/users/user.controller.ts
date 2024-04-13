d61e9309477a26c8e556945bea10fc0de118ce69
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-14 05:53:00 +0700

    Refactor user.controller.ts to import CreateUserDto from the correct location

--- src/apps/accounts/users/user.controller.ts
+++ src/apps/accounts/users/user.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Post, Headers, Get, Patch, Put, Param } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 
-import { CreateUserDto, UpdateUserDto } from 'src/validators';
+
 import * as _ from 'underscore';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto';
 @Controller('users')
 export class UserController {
   constructor(private readonly _userService: UserService) {}
