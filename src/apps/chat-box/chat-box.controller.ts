4877bb4dac72222f2fa10832b9c31028f4d53a23
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-14 05:53:19 +0700

    Refactor chat-box.controller.ts to import CreateMessageDto from the correct location

--- src/apps/chat-box/chat-box.controller.ts
+++ src/apps/chat-box/chat-box.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post, Headers, UsePipes, Param, Query, Get, Delete, Put } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 
-import { CreateMessageDto } from 'src/validators';
+
 import _ from 'underscore';
 import { ChatBoxService } from './chat-box.service';
 import { MessageService } from '../messages/message.service';
+import { CreateMessageDto } from '../messages/dtos';
 
 @Controller('chat')
 export class ChatBoxController {
