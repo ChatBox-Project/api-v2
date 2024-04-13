115d38801dd0fee97c2c90715add87494e0ee68b
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-13 19:27:00 +0700

    Refactor MessageModule providers in message.module.ts

--- src/modules/messages/message.module.ts
+++ src/modules/messages/message.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageController } from 'src/controllers';
 import { entities } from 'src/entities/entity.provider';
-import { AccountRepository, ChatBoxRepository, MessageRepository, UserRepository } from 'src/repositories';
-import { AppGateWay, MessageService, ResponseService } from 'src/services';
+import { AppGateWay, ChatBoxService, MessageService, ResponseService } from 'src/services';
 // import { AppGateWayModule } from '../gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
   controllers: [MessageController],
-  providers: [MessageService, MessageRepository, ChatBoxRepository, UserRepository, AccountRepository, ResponseService, AppGateWay],
+  providers: [MessageService, AppGateWay, ResponseService, ChatBoxService],
 })
 export class MessageModule {}
