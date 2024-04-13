89d13ada61b1b423cbaa68abacb8e01dbfd44183
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-14 05:53:40 +0700

    Refactor import statements to use correct locations in repository.provider.ts

--- src/common/providers/repository.provider.ts
+++ src/common/providers/repository.provider.ts
@@ -1,13 +1,11 @@
 import { Provider } from '@nestjs/common';
-import {
-  AccountRepository,
-  ChatBoxRepository,
-  ChatGroupRepository,
-  GroupLeaderPermissionRepository,
-  MessageRepository,
-  PermissionRepository,
-  RoleRepository,
-} from 'src/common/repositories';
+import { AccountRepository } from 'src/apps/accounts/account.repository';
+import { RoleRepository } from 'src/apps/accounts/roles/role.repository';
+import { ChatBoxRepository } from 'src/apps/chat-box/chat-box.entity';
+import { ChatGroupRepository } from 'src/apps/chat-groups/chat-group.repository';
+import { MessageRepository } from 'src/apps/messages/message.repository';
+import { GroupLeaderPermissionRepository } from 'src/apps/permissions/group-leader/group-leader-permission.repository';
+import { PermissionRepository } from 'src/apps/permissions/permission.repository';
 import { BaseRepository } from 'src/common/repositories';
 import { DataSource } from 'typeorm';
 
