2f3112869f4bae88244476e02f0566c51ea24531
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-14 05:53:34 +0700

    Import AccountRepository from the correct location in otp.module.ts

--- src/apps/otp/otp.module.ts
+++ src/apps/otp/otp.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpController } from 'src/apps/otp/otp.controller';
 import { entities } from 'src/common/entities/entity.provider';
-
-
-
 import { OtpService } from './otp.service';
 import { ResponseService } from 'src/common/res';
+import { AccountRepository } from '../accounts/account.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
