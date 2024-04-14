a706dade1a1ac67a9404af487c4a8dc065252bee
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-14 18:56:49 +0700

    Add MongooseDatabaseModule and PostgresDatabaseModule for database configuration

---
+++ src/common/database/postgres/postgres.database.module.ts
@@ -0,0 +1,39 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_SYNCHRONIZE: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DATABASE_HOST'),
+        port: configService.getOrThrow('DATABASE_PORT'),
+        database: configService.getOrThrow('DATABASE_NAME'),
+        username: configService.getOrThrow('DATABASE_USERNAME'),
+        password: configService.getOrThrow('DATABASE_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: configService.getOrThrow('DATABASE_SYNCHRONIZE'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [],
+})
+export class PostgresDatabaseModule {}
