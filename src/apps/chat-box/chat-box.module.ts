7bbaa4b1d5a951856020d67ef1746706b1e897b4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-15 22:19:58 +0700

    Remove commented out code and import GatewayModule in chat-box.module.ts

--- src/apps/chat-box/chat-box.module.ts
+++ src/apps/chat-box/chat-box.module.ts
@@ -6,10 +6,11 @@ import { ChatBoxService } from './chat-box.service';
 import { ResponseService } from 'src/common/res';
 import { MessageService } from '../messages/message.service';
 import { ChatBoxController } from './chat-box.controller';
-import { GatewayModule } from '../gateways/gateway.module';
+// import { GatewayModule } from '../gateways/gateway.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature(entities), GatewayModule],
+  imports: [TypeOrmModule.forFeature(entities)],
   providers: [ChatBoxService, ResponseService, MessageService],
   controllers: [ChatBoxController],
   exports: [ChatBoxService],
