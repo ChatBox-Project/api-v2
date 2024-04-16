55c5c7432e4ab90dc33c34492904920c3bd465ad
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:09:46 +0700

    Add user-related API endpoints to UserController

--- src/apps/accounts/users/user.controller.ts
+++ src/apps/accounts/users/user.controller.ts
@@ -3,9 +3,25 @@ import { ApiOkResponse } from '@nestjs/swagger';
 
 import * as _ from 'underscore';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto';
+import { CreateUserDto, UpdateUserDto } from './dto';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
+  @Post()
+  @ApiOkResponse({ description: 'Create user' })
+  public async create(@Body() createUserDto: CreateUserDto, @Headers() _headers: any) {
+    return await this.userService.create(_headers.token, createUserDto);
+  }
+  @Get()
+  @ApiOkResponse({ description: 'Get user' })
+  public async getUserId(@Headers() _header: any) {
+    return await this.userService.getUserId(_header.token);
+  }
+  @Put()
+  @ApiOkResponse({ description: 'Update user' })
+  public async update(@Body() updateUserDto: UpdateUserDto, @Headers() _headers: any) {
+    return await this.userService.updateUser(_headers.token, updateUserDto);
+  }
 }
