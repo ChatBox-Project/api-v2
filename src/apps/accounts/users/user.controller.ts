c167680e091621d4b6f934ac37eae382dd5ca764
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 15:27:04 +0700

    Refactor UserController to use any type for createUserDto in create method

--- src/apps/accounts/users/user.controller.ts
+++ src/apps/accounts/users/user.controller.ts
@@ -11,7 +11,8 @@ export class UserController {
 
   @Post()
   @ApiOkResponse({ description: 'Create user' })
-  public async create(@Body() createUserDto: CreateUserDto, @Headers() _headers: any) {
+  public async create(@Body() createUserDto: any, @Headers() _headers: any) {
+    console.log('createUserDto', createUserDto)
     return await this.userService.create(_headers.token, createUserDto);
   }
   @Get()
