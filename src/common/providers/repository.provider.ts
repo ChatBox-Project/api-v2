b890cb43cd3a5972810d3954df576cb10164bace
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:11:27 +0700

    Refactor repository provider to remove unused imports and repositories

--- src/common/providers/repository.provider.ts
+++ src/common/providers/repository.provider.ts
@@ -1,14 +1,11 @@
 import { Provider } from '@nestjs/common';
 import { AccountRepository } from 'src/apps/accounts/account.repository';
-import { RoleRepository } from 'src/apps/accounts/roles/role.repository';
 
-import { MessageRepository } from 'src/apps/messages/message.repository';
-import { GroupLeaderPermissionRepository } from 'src/apps/permissions/group-leader/group-leader-permission.repository';
-import { PermissionRepository } from 'src/apps/permissions/permission.repository';
+
 import { BaseRepository } from 'src/common/repositories-postgres';
 import { DataSource } from 'typeorm';
 
-const repositories = [AccountRepository, BaseRepository, MessageRepository, PermissionRepository, RoleRepository, GroupLeaderPermissionRepository];
+const repositories = [AccountRepository, BaseRepository];
 
 const RepositoriesProvider: Provider[] = [];
 
