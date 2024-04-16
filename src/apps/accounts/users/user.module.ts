fbc16e7493910cd7ac93498c34e55bb97985b85a
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:09:50 +0700

    Add UserRepository to handle user-related operations

--- src/apps/accounts/users/user.module.ts
+++ src/apps/accounts/users/user.module.ts
@@ -7,10 +7,11 @@ import { UserService } from './user.service';
 import { ResponseService } from 'src/common/res';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/common/models/user.model';
+import { UserRepository } from 'src/common';
 
 @Module({
-  imports: [MongooseModule.forFeature()],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), TypeOrmModule.forFeature(entities)],
   controllers: [UserController],
-  providers: [UserService, ResponseService],
+  providers: [UserService, ResponseService, UserRepository],
 })
 export class UserModule {}
