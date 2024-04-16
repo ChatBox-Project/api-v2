ef3505f0662b3712f272ee60118e210e07e434ad
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 23:26:49 +0700

    Refactor UserController to add new endpoints and update existing ones

--- src/apps/accounts/users/user.controller.ts
+++ src/apps/accounts/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Headers, Get, Patch, Put, Param } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Get, Patch, Put, Param, Query } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 
 import * as _ from 'underscore';
@@ -12,16 +12,35 @@ export class UserController {
   @Post()
   @ApiOkResponse({ description: 'Create user' })
   public async create(@Body() createUserDto: any, @Headers() _headers: any) {
-    return  await this.userService.create(_headers.token, createUserDto);
+    return await this.userService.create(_headers.token, createUserDto);
   }
-  @Get()
-  @ApiOkResponse({ description: 'Get user' })
-  public async getUserId(@Headers() _header: any) {
-    return await this.userService.getUserId(_header.token);
+  @Get(':id')
+  @ApiOkResponse({ description: 'Get user by id' })
+  public async getUserId(@Param('id') _id: string) {
+    return await this.userService.getUserId(_id);
   }
   @Put()
   @ApiOkResponse({ description: 'Update user' })
   public async update(@Body() updateUserDto: UpdateUserDto, @Headers() _headers: any) {
     return await this.userService.updateUser(_headers.token, updateUserDto);
+  }
+
+  @Put('add-friend/:id')
+  @ApiOkResponse({ description: 'Add friend' })
+  public async addFriend(@Param('id') _id: string, @Headers() _headers: any) {
+    // console.log('id', _id, 'headers', _headers.token)
+    return await this.userService.addFriend(_headers.token, _id);
+  }
+  @Put('acp-friend/:id')
+  public async acceptFriend(@Param('id') _id: string, @Headers() _headers: any) {
+    // console.log('id', _id, 'headers', _headers.token)
+    return await this.userService.acceptFriend(_headers.token, _id);
+  }
+
+  @Get('search/:phone')
+  @ApiOkResponse({ description: 'Search user by phone' })
+  public async search(@Param('phone') phone: string, @Headers() _headers: any) {
+    // console.log('phone', phone, 'headers', _headers.token)
+    return await this.userService.search(_headers.token, phone);
   }
 }
--- src/apps/accounts/users/user.service.ts
+++ src/apps/accounts/users/user.service.ts
@@ -56,21 +56,17 @@ export class UserService {
       });
     }
   }
-  public async getUserId(token: string) {
+  public async getUserId(id: string) {
     try {
-      const holderAccount = await this.findAccountByToken(token);
-
-      if (!holderAccount) {
+      const user = await this.userModel.findOne({ _id: id }).lean();
+      if (!user) {
         throw new ErrorResponse({
-          ...new NotFoundException('Account is not exists'),
-          errorCode: 'ACCOUNT_NOT_EXISTS',
+          ...new NotFoundException('User is not exists'),
+          errorCode: 'USER_NOT_EXISTS',
         });
       }
-
-      const user = await this.userModel.findOne({ accountId: holderAccount.id });
       // console.log('user', user)
       const metadata = { user };
-
       return this._response.createResponse(200, 'success', metadata);
     } catch (error) {
       throw new ErrorResponse({
@@ -98,6 +94,125 @@ export class UserService {
       throw new ErrorResponse({
         ...new BadRequestException('Token is not exists'),
         errorCode: 'UPDATE_USER_FAIL',
+      });
+    }
+  }
+
+  public async addFriend(token: string, friendId: string): Promise<unknown> {
+    try {
+      const holderAccount = await this.findAccountByToken(token);
+      // console.log('holderAccount', holderAccount);
+
+      if (!holderAccount) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Account is not exists'),
+          errorCode: 'ACCOUNT_NOT_EXISTS',
+        });
+      }
+      const holderUser = await this.userModel.findOne({ accountId: holderAccount.id });
+      console.log('holderUser', holderUser);
+
+      // check friend is exists in holderUser array
+      const isFriendExists = holderUser.friends.find((fr) => fr.user_id === friendId);
+      // console.log('isFriendExists', isFriendExists);
+      if (isFriendExists) {
+        throw new ErrorResponse({
+          ...new ConflictException('Friend is exists'),
+          errorCode: 'FRIEND_IS_EXISTS',
+        });
+      }
+      // add friend
+      const friend = await this.userModel.findByIdAndUpdate(
+        friendId,
+        {
+          $push: { friends: { user_id: holderUser.id, status: 'PENDING' } },
+        },
+        { new: true },
+      );
+      // console.log('friend', friend);
+
+      const friendHolderUser = await this.userModel.findByIdAndUpdate(
+        holderUser.id,
+        {
+          $push: { friends: { user_id: friendId, status: 'PENDING' } },
+        },
+        { new: true },
+      );
+
+      const metadata = { friend, friendHolderUser };
+      return this._response.createResponse(200, 'success', metadata);
+      return;
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException('Add friend failed'),
+        errorCode: 'ADD_FRIEND_FAILED',
+      });
+    }
+  }
+  // bug
+  public async acceptFriend(token: string, friendId: string): Promise<unknown> {
+    try {
+      const holderAccount = await this.findAccountByToken(token);
+      // console.log('holderAccount', holderAccount);
+
+      if (!holderAccount) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Account is not exists'),
+          errorCode: 'ACCOUNT_NOT_EXISTS',
+        });
+      }
+      const holderUser = await this.userModel.findOne({ accountId: holderAccount.id });
+      console.log('holderUser', holderUser);
+
+      // check friend is exists in holderUser array
+      const isFriendExists = holderUser.friends.find((fr) => fr.user_id === friendId);
+      console.log('isFriendExists', isFriendExists);
+
+      // accept friend
+
+      return;
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException('Accept friend failed'),
+        errorCode: 'ACCEPT_FRIEND_FAILED',
+      });
+    }
+  }
+  public async search(token: string, phone: string) {
+    try {
+      const account = await this.findAccountByToken(token);
+      if (!account) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Account is not exists'),
+          errorCode: 'ACCOUNT_NOT_EXISTS',
+        });
+      }
+      const foundAccount = await this.accountRepository.findOne({ where: { phoneNumber: phone } });
+      // console.log('foundAccount', foundAccount);
+
+      if (!foundAccount) {
+        throw new ErrorResponse({
+          ...new BadRequestException('Account is not exists'),
+          errorCode: 'ACCOUNT_NOT_EXISTS',
+        });
+      }
+
+      const foundUser = await this.userModel.findOne({ accountId: foundAccount.id });
+      // console.log('foundUser', foundUser);
+
+      if (!foundUser) {
+        throw new ErrorResponse({
+          ...new BadRequestException('User is not exists'),
+          errorCode: 'USER_NOT_EXISTS',
+        });
+      }
+
+      const metadata = { foundUser };
+      return this._response.createResponse(200, 'success', metadata);
+    } catch (error) {
+      throw new ErrorResponse({
+        ...new BadRequestException('Search failed'),
+        errorCode: 'SEARCH_FAILED',
       });
     }
   }
