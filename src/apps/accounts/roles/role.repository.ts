8f56e0a275cba7b4ac3d1300a2d52a8d5b324e05
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:09:02 +0700

    Remove RoleRepository and unused files

--- src/apps/accounts/roles/role.repository.ts
+++
@@ -1,4 +0,0 @@
-import { BaseRepository } from '../../../common/repositories-postgres/base.repository';
-import { RoleEntity } from '../../../common/entities/role.entity';
-
-export class RoleRepository extends BaseRepository<RoleEntity> {}
