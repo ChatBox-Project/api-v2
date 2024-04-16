e2969f8fa5f2046f9bacd6009fe9cd71bc687b01
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-17 02:58:09 +0700

    Refactor MessageController to add saveMessage and getAllMessagesInConversation endpoints

--- src/apps/messages/message.controller.ts
+++ src/apps/messages/message.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Param, Post } from '@nestjs/common';
 
 import _ from 'underscore';
 import { MessageService } from './message.service';
-@Controller('message')
+import { CreateMessageDto } from './dtos';
+@Controller('messages')
 export class MessageController {
   constructor(private _messageService: MessageService) {}
 
-  // @Post()
-  // public async saveMessage(@Headers() _headers: any, @Body() _message: CreateMessageDto): Promise<unknown> {
-  //   const message = await this._messageService.createMessage(_headers.token, _message);
-  //   return _.omit(message, 'message');
-  // }
+  @Post(':id')
+  public async saveMessage(@Headers() _headers: any, @Body() _message: any, @Param('id') conversationId: string): Promise<unknown> {
+    // console.log('saveMessage', _message,"id::::", conversationId, _headers.token);
+    return await this._messageService.save(_headers.token, conversationId, _message);
+  }
+  @Get(':id')
+  public async getAllMessagesInConversation(@Param('id') conversationId: string, @Body() data: any): Promise<unknown> {
+    // console.log('getAllMessagesInConversation', conversationId, data);
+    return await this._messageService.getMessageByConversationId(conversationId, { ...data });
+  }
 }
