263d5a24522104494308df9be69d454f8b24abd0
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:10:44 +0700

    Remove unused entities and associations from common/entities/index.ts

--- src/common/entities/index.ts
+++ src/common/entities/index.ts
@@ -1,9 +1,2 @@
 export * from './account.entity';
-export * from './chat-box.entity';
-export * from './entity.provider';
-export * from './permission.entity';
-export * from './user.base.entity';
-export * from './message.entity';
-export * from './chat-group.entity';
-export * from './group-leader-permission.entity';
 
