2e462106c3fd4c2673bd59859f53ed87b9033442
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-16 10:10:38 +0700

    Refactor entity.provider.ts to remove unused imports and associations

--- src/common/entities/entity.provider.ts
+++ src/common/entities/entity.provider.ts
@@ -1,20 +1,3 @@
 import { AccountEntity } from './account.entity';
-import { ChatBoxEntity } from './chat-box.entity';
-import { ChatGroupEntity } from './chat-group.entity';
-import { GroupLeaderPermissionEntity } from './group-leader-permission.entity';
-import { MessageEntity } from './message.entity';
-import { PermissionEntity } from './permission.entity';
-import { RoleEntity } from './role.entity';
-import { UserEntity } from './user.base.entity';
 
-export const entities = [
-  AccountEntity,
-  ChatBoxEntity,
-  GroupLeaderPermissionEntity,
-  RoleEntity,
-  MessageEntity,
-  PermissionEntity,
-  ChatGroupEntity,
-  UserEntity,
-  
-];
+export const entities = [AccountEntity];
